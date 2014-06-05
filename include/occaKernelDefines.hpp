bf280438f8d69d3f1b9011e40a095470368f27db
David Medina <usdmgz@houcy1-n-cp031j07.americas.shell.com>
2014-06-05 16:42:06 -0500

    A wild setuprc had appeared

--- include/occaKernelDefines.hpp
+++ include/occaKernelDefines.hpp
@@ -1,5 +1,5 @@
 namespace occa {
-    extern char occaOpenMPDefines[5648];
+    extern char occaOpenMPDefines[5660];
     extern char occaOpenCLDefines[3003];
     extern char occaCUDADefines[2891];
 }
