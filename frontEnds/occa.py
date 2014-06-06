1dfa6c14d836a59bdd6851f403917815c5d34a70
dsm5 <dmed256@gmail.com>
2014-06-06 16:17:14 -0500

    Added main.py and still working on occa.py

--- frontEnds/occa.py
+++ frontEnds/occa.py
@@ -1,89 +1,111 @@
 from ctypes import *
+from array import *
 
 libocca = CDLL('libocca.so', RTLD_GLOBAL)
 
 class device:
     def mode(self):
-        libocca.occaDeviceMode(self.device)
+        return libocca.occaDeviceMode(self.cDevice)
+
+    def __init__(self, mode, platformID, deviceID):
+        self.cDevice = libocca.occaGetDevice(mode, platformID, deviceID)
 
     def setup(self, mode, platformID, deviceID):
-        self.device = libocca.occaGetDevice(mode, platformID, deviceID)
+        self.cDevice = libocca.occaGetDevice(mode, platformID, deviceID)
 
     def setOmpCompiler(self, compiler):
-        libocca.occaDeviceSetOmpCompiler(self.device, compiler)
+        libocca.occaDeviceSetOmpCompiler(self.cDevice, compiler)
 
     def setOmpCompilerFlags(self, compilerFlags):
-        libocca.occaDeviceSetOmpCompilerFlags(self.device, compilerFlags)
+        libocca.occaDeviceSetOmpCompilerFlags(self.cDevice, compilerFlags)
 
     def setCudaCompiler(self, compiler):
-        libocca.occaDeviceSetCudaCompiler(self.device, compiler)
+        libocca.occaDeviceSetCudaCompiler(self.cDevice, compiler)
 
     def setCudaCompilerFlags(self, compilerFlags):
-        libocca.occaDeviceSetCudaCompilerFlags(self.device, compilerFlags)
+        libocca.occaDeviceSetCudaCompilerFlags(self.cDevice, compilerFlags)
 
     def malloc(self, byteCount, source):
-        return memory(libocca.occaDevicMalloc(self.device,
+        return memory(libocca.occaDevicMalloc(self.cDevice,
                                               byteCount,
                                               source))
 
     def genStream(self):
-        return libocca.occaGenStream(self.device)
+        return libocca.occaGenStream(self.cDevice)
 
     def getStream(self):
-        return libocca.occaGetStream(self.device)
+        return libocca.occaGetStream(self.cDevice)
 
     def setStream(self):
-        return libocca.occaSetStream(self.device)
+        return libocca.occaSetStream(self.cDevice)
 
     def free(self):
-        return libocca.occaDeviceFree(self.device)
+        return libocca.occaDeviceFree(self.cDevice)
 
 class kernelInfo:
     def addDefine(self):
         pass
 
 class kernel:
-
     def mode(self):
-        pass
+        return libocca.occaKernelMode(self.cKernel)
+
+    def __init__(self, cKernel):
+        self.cKernel = cKernel
 
     def preferredDimSize(self):
-        pass
+        return libocca.occaKernelPreferredDimSize(self.cKernel)
 
-    def setWorkingDims(self):
-        pass
+    def setWorkingDims(self, dims, itemsPerGroup, groups):
+
+
+        # occaDims = ?
+        return libocca.occaKernelSetWorkingDims(self.cKernel)
+
 
     # (self) Operator
 
     def timeTaken(self):
-        pass
+        return libocca.occaKernelTimeTaken(self.cKernel)
 
     def free(self):
-        pass
+        libocca.occaKernelFree(self.cKernel)
 
 class memory:
-
     def mode(self):
+        return libocca.occaMemoryMode(self.cMemory)
+
+    def __init__(self, cMemory):
+        self.cMemory = cMemory
+
+    def copyTo(self, dest, byteCount = 0, offset = 0):
+        if dest.__class__ is array:
+            pass
+        elif dest.__class__ is memory:
+            pass
+        else:
+            print "Error"
+
+    def copyFrom(self, src, byteCount = 0, offset = 0):
         pass
 
-    def copyTo(self):
+    def asyncCopyTo(self, dest, byteCount = 0, offset = 0):
         pass
 
-    def copyFrom(self):
+    def asyncCopyFrom(self, src, byteCount = 0, offset = 0):
         pass
 
-    def asyncCopyTo(self):
-        pass
-
-    def asyncCopyFrom(self):
-        pass
-
-    def swap(self):
-        pass
+    def swap(self, m):
+        self.cMemory, m.cMemory = m.cMemory, self.cMemory
 
     def free(self):
-        pass
+        libocca.occaMemoryFree(self.cMemory)
 
-dev = device()
+a = array('f', [0,1])
+d = device("OpenMP", 0, 0)
 
-dev.setup("OpenMP", 0, 0)
+if a.__class__ is array:
+    print 1
+
+if d.__class__ is device:
+    print 1
