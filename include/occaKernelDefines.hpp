cfa864aaf1792bfd58359c70a03e654a58b0c7f0
dsm5 <dsm5@mal>
2014-08-15 14:16:20 -0500

    [CUDA] Compiles with the math functions

--- include/occaKernelDefines.hpp
+++ include/occaKernelDefines.hpp
@@ -2,7 +2,7 @@ namespace occa {
     extern char occaPthreadsDefines[7848];
     extern char occaOpenMPDefines[7624];
     extern char occaOpenCLDefines[5047];
-    extern char occaCUDADefines[9692];
+    extern char occaCUDADefines[10337];
     extern char occaCOIDefines[7865];
     extern char occaCOIMain[37440];
 }
