a8395b97c7e3b86057061c5163b0ca65094c3c1b
kummer <kummerfdy.tu-darmstadt.de>
2014-08-18 11:04:18 -0500

    starting C# wrapper library

---
+++ visual_studio/liboccaSharp/occaDim.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace liboccaSharp {
+    public struct occaDim {
+        public UIntPtr x;
+        public UIntPtr y;
+        public UIntPtr z;
+    }
+}
