f3fbe9fd2c96b75160127bfee3a0d13d4dbd7a34
Lucas C Wilcox <lucas@swirlee.com>
2014-09-03 11:58:33 -0700

    [Fortran] Add macros for Fortran name mangling

--- src/occa_fc.c
+++ src/occa_fc.c
@@ -90,48 +90,103 @@ do {\
   if (a != b) free(b);\
 } while (0)
 
+#define OCCA_F2C_GLOBAL_(name,NAME) name##_
+
+#define  OCCAINT_FC                      OCCA_F2C_GLOBAL_(occaInt_fc                    , OCCAINT_FC)
+#define  OCCAUINT_FC                     OCCA_F2C_GLOBAL_(occaUInt_fc                   , OCCAUINT_FC)
+#define  OCCACHAR_FC                     OCCA_F2C_GLOBAL_(occaChar_fc                   , OCCACHAR_FC)
+#define  OCCAUCHAR_FC                    OCCA_F2C_GLOBAL_(occaUChar_fc                  , OCCAUCHAR_FC)
+#define  OCCASHORT_FC                    OCCA_F2C_GLOBAL_(occaShort_fc                  , OCCASHORT_FC)
+#define  OCCAUSHORT_FC                   OCCA_F2C_GLOBAL_(occaUShort_fc                 , OCCAUSHORT_FC)
+#define  OCCALONG_FC                     OCCA_F2C_GLOBAL_(occaLong_fc                   , OCCALONG_FC)
+#define  OCCAULONG_FC                    OCCA_F2C_GLOBAL_(occaULong_fc                  , OCCAULONG_FC)
+#define  OCCAFLOAT_FC                    OCCA_F2C_GLOBAL_(occaFloat_fc                  , OCCAFLOAT_FC)
+#define  OCCADOUBLE_FC                   OCCA_F2C_GLOBAL_(occaDouble_fc                 , OCCADOUBLE_FC)
+#define  OCCASTRING_FC                   OCCA_F2C_GLOBAL_(occaString_fc                 , OCCASTRING_FC)
+#define  OCCADEVICEMODE_FC               OCCA_F2C_GLOBAL_(occaDeviceMode_fc             , OCCADEVICEMODE_FC)
+#define  OCCADEVICESETCOMPILER_FC        OCCA_F2C_GLOBAL_(occaDeviceSetCompiler_fc      , OCCADEVICESETCOMPILER_FC)
+#define  OCCADEVICESETCOMPILERFLAGS_FC   OCCA_F2C_GLOBAL_(occaDeviceSetCompilerFlags_fc , OCCADEVICESETCOMPILERFLAGS_FC)
+#define  OCCAGETDEVICE_FC                OCCA_F2C_GLOBAL_(occaGetDevice_fc              , OCCAGETDEVICE_FC)
+#define  OCCABUILDKERNELFROMSOURCE_FC    OCCA_F2C_GLOBAL_(occaBuildKernelFromSource_fc  , OCCABUILDKERNELFROMSOURCE_FC)
+#define  OCCABUILDKERNELFROMBINARY_FC    OCCA_F2C_GLOBAL_(occaBuildKernelFromBinary_fc  , OCCABUILDKERNELFROMBINARY_FC)
+#define  OCCABUILDKERNELFROMLOOPY_FC     OCCA_F2C_GLOBAL_(occaBuildKernelFromLoopy_fc   , OCCABUILDKERNELFROMLOOPY_FC)
+#define  OCCADEVICEMALLOC_FC             OCCA_F2C_GLOBAL_(occaDeviceMalloc_fc           , OCCADEVICEMALLOC_FC)
+#define  OCCADEVICEFLUSH_FC              OCCA_F2C_GLOBAL_(occaDeviceFlush_fc            , OCCADEVICEFLUSH_FC)
+#define  OCCADEVICEFINISH_FC             OCCA_F2C_GLOBAL_(occaDeviceFinish_fc           , OCCADEVICEFINISH_FC)
+#define  OCCADEVICEGENSTREAM_FC          OCCA_F2C_GLOBAL_(occaDeviceGenStream_fc        , OCCADEVICEGENSTREAM_FC)
+#define  OCCADEVICEGETSTREAM_FC          OCCA_F2C_GLOBAL_(occaDeviceGetStream_fc        , OCCADEVICEGETSTREAM_FC)
+#define  OCCADEVICESETSTREAM_FC          OCCA_F2C_GLOBAL_(occaDeviceSetStream_fc        , OCCADEVICESETSTREAM_FC)
+#define  OCCADEVICETAGSTREAM_FC          OCCA_F2C_GLOBAL_(occaDeviceTagStream_fc        , OCCADEVICETAGSTREAM_FC)
+#define  OCCADEVICETIMEBETWEENTAGS_FC    OCCA_F2C_GLOBAL_(occaDeviceTimeBetweenTags_fc  , OCCADEVICETIMEBETWEENTAGS_FC)
+#define  OCCADEVICESTREAMFREE_FC         OCCA_F2C_GLOBAL_(occaDeviceStreamFree_fc       , OCCADEVICESTREAMFREE_FC)
+#define  OCCADEVICEFREE_FC               OCCA_F2C_GLOBAL_(occaDeviceFree_fc             , OCCADEVICEFREE_FC)
+#define  OCCAKERNELMODE_FC               OCCA_F2C_GLOBAL_(occaKernelMode_fc             , OCCAKERNELMODE_FC)
+#define  OCCAKERNELPREFERREDDIMSIZE_FC   OCCA_F2C_GLOBAL_(occaKernelPreferredDimSize_fc , OCCAKERNELPREFERREDDIMSIZE_FC)
+#define  OCCAKERNELSETWORKINGDIMS_FC     OCCA_F2C_GLOBAL_(occaKernelSetWorkingDims_fc   , OCCAKERNELSETWORKINGDIMS_FC)
+#define  OCCAKERNELSETALLWORKINGDIMS_FC  OCCA_F2C_GLOBAL_(occaKernelSetAllWorkingDims_fc, OCCAKERNELSETALLWORKINGDIMS_FC)
+#define  OCCAKERNELTIMETAKEN_FC          OCCA_F2C_GLOBAL_(occaKernelTimeTaken_fc        , OCCAKERNELTIMETAKEN_FC)
+#define  OCCAGENARGUMENTLIST_FC          OCCA_F2C_GLOBAL_(occaGenArgumentList_fc        , OCCAGENARGUMENTLIST_FC)
+#define  OCCAARGUMENTLISTCLEAR_FC        OCCA_F2C_GLOBAL_(occaArgumentListClear_fc      , OCCAARGUMENTLISTCLEAR_FC)
+#define  OCCAARGUMENTLISTFREE_FC         OCCA_F2C_GLOBAL_(occaArgumentListFree_fc       , OCCAARGUMENTLISTFREE_FC)
+#define  OCCAARGUMENTLISTADDARG_FC       OCCA_F2C_GLOBAL_(occaArgumentListAddArg_fc     , OCCAARGUMENTLISTADDARG_FC)
+#define  OCCAKERNELRUN__FC               OCCA_F2C_GLOBAL_(occaKernelRun__fc             , OCCAKERNELRUN__FC)
+#define  OCCAKERNELFREE_FC               OCCA_F2C_GLOBAL_(occaKernelFree_fc             , OCCAKERNELFREE_FC)
+#define  OCCAGENKERNELINFO_FC            OCCA_F2C_GLOBAL_(occaGenKernelInfo_fc          , OCCAGENKERNELINFO_FC)
+#define  OCCAKERNELINFOADDDEFINE_FC      OCCA_F2C_GLOBAL_(occaKernelInfoAddDefine_fc    , OCCAKERNELINFOADDDEFINE_FC)
+#define  OCCAKERNELINFOFREE_FC           OCCA_F2C_GLOBAL_(occaKernelInfoFree_fc         , OCCAKERNELINFOFREE_FC)
+#define  OCCAMEMORYMODE_FC               OCCA_F2C_GLOBAL_(occaMemoryMode_fc             , OCCAMEMORYMODE_FC)
+#define  OCCACOPYMEMTOMEM_FC             OCCA_F2C_GLOBAL_(occaCopyMemToMem_fc           , OCCACOPYMEMTOMEM_FC)
+#define  OCCACOPYPTRTOMEM_FC             OCCA_F2C_GLOBAL_(occaCopyPtrToMem_fc           , OCCACOPYPTRTOMEM_FC)
+#define  OCCACOPYMEMTOPTR_FC             OCCA_F2C_GLOBAL_(occaCopyMemToPtr_fc           , OCCACOPYMEMTOPTR_FC)
+#define  OCCAASYNCCOPYMEMTOMEM_FC        OCCA_F2C_GLOBAL_(occaAsyncCopyMemToMem_fc      , OCCAASYNCCOPYMEMTOMEM_FC)
+#define  OCCAASYNCCOPYPTRTOMEM_FC        OCCA_F2C_GLOBAL_(occaAsyncCopyPtrToMem_fc      , OCCAASYNCCOPYPTRTOMEM_FC)
+#define  OCCAASYNCCOPYMEMTOPTR_FC        OCCA_F2C_GLOBAL_(occaAsyncCopyMemToPtr_fc      , OCCAASYNCCOPYMEMTOPTR_FC)
+#define  OCCAMEMORYSWAP_FC               OCCA_F2C_GLOBAL_(occaMemorySwap_fc             , OCCAMEMORYSWAP_FC)
+#define  OCCAMEMORYFREE_FC               OCCA_F2C_GLOBAL_(occaMemoryFree_fc             , OCCAMEMORYFREE_FC)
+
+
 #  ifdef __cplusplus
 extern "C" {
 #  endif
 
   //---[ TypeCasting ]------------------
-  occaType occaInt_fc(int value){
+  occaType OCCAINT_FC(int value){
     return occaInt(value);
   }
 
-  occaType occaUInt_fc(unsigned int value){
+  occaType OCCAUINT_FC(unsigned int value){
     return occaUInt(value);
   }
 
-  occaType occaChar_fc(char value){
+  occaType OCCACHAR_FC(char value){
     return occaChar(value);
   }
-  occaType occaUChar_fc(unsigned char value){
+  occaType OCCAUCHAR_FC(unsigned char value){
     return occaUChar(value);
   }
 
-  occaType occaShort_fc(short value){
+  occaType OCCASHORT_FC(short value){
     return occaShort(value);
   }
-  occaType occaUShort_fc(unsigned short value){
+  occaType OCCAUSHORT_FC(unsigned short value){
     return occaUShort(value);
   }
 
-  occaType occaLong_fc(long value){
+  occaType OCCALONG_FC(long value){
     return occaLong(value);
   }
-  occaType occaULong_fc(unsigned long value){
+  occaType OCCAULONG_FC(unsigned long value){
     return occaULong(value);
   }
 
-  occaType occaFloat_fc(float value){
+  occaType OCCAFLOAT_FC(float value){
     return occaFloat(value);
   }
-  occaType occaDouble_fc(double value){
+  occaType OCCADOUBLE_FC(double value){
     return occaDouble(value);
   }
 
-  occaType occaString_fc(char *str OCCA_F2C_LSTR(str_l)
+  occaType OCCASTRING_FC(char *str OCCA_F2C_LSTR(str_l)
                          OCCA_F2C_RSTR(str_l)){
     char *str_c;
     OCCA_F2C_ALLOC_STR(str, str_l, str_c);
@@ -146,11 +201,11 @@ extern "C" {
 
 
   //---[ Device ]-----------------------
-  const char* occaDeviceMode_fc(occaDevice device){ // [-]
+  const char* OCCADEVICEMODE_FC(occaDevice device){ // [-]
     occaDeviceMode(device);
   }
 
-  void occaDeviceSetCompiler_fc(occaDevice device,
+  void OCCADEVICESETCOMPILER_FC(occaDevice device,
                                 const char *compiler OCCA_F2C_LSTR(compiler_l)
                                 OCCA_F2C_RSTR(compiler_l)){
     char *compiler_c;
@@ -161,7 +216,7 @@ extern "C" {
     OCCA_F2C_FREE_STR(compiler, compiler_c);
   }
 
-  void occaDeviceSetCompilerFlags_fc(occaDevice device,
+  void OCCADEVICESETCOMPILERFLAGS_FC(occaDevice device,
                                      const char *compilerFlags OCCA_F2C_LSTR(compilerFlags_l)
                                      OCCA_F2C_RSTR(compilerFlags_l)){
     char *compilerFlags_c;
@@ -172,7 +227,7 @@ extern "C" {
     OCCA_F2C_FREE_STR(compilerFlags, compilerFlags_c);
   }
 
-  occaDevice occaGetDevice_fc(const char *mode OCCA_F2C_LSTR(mode_l),
+  occaDevice OCCAGETDEVICE_FC(const char *mode OCCA_F2C_LSTR(mode_l),
                               int arg1, int arg2
                               OCCA_F2C_RSTR(mode_l)){
     char *mode_c;
@@ -185,7 +240,7 @@ extern "C" {
     return ret;
   }
 
-  occaKernel occaBuildKernelFromSource_fc(occaDevice device,
+  occaKernel OCCABUILDKERNELFROMSOURCE_FC(occaDevice device,
                                           const char *filename     OCCA_F2C_LSTR(filename_l),
                                           const char *functionName OCCA_F2C_LSTR(functionName_l),
                                           occaKernelInfo info
@@ -203,7 +258,7 @@ extern "C" {
     return ret;
   }
 
-  occaKernel occaBuildKernelFromBinary_fc(occaDevice device,
+  occaKernel OCCABUILDKERNELFROMBINARY_FC(occaDevice device,
                                           const char *filename     OCCA_F2C_LSTR(filename_l),
                                           const char *functionName OCCA_F2C_LSTR(functionName_l)
                                           OCCA_F2C_RSTR(filename_l)
@@ -218,7 +273,7 @@ extern "C" {
     OCCA_F2C_FREE_STR(functionName, functionName_c);
   }
 
-  occaKernel occaBuildKernelFromLoopy_fc(occaDevice device,
+  occaKernel OCCABUILDKERNELFROMLOOPY_FC(occaDevice device,
                                          const char *filename     OCCA_F2C_LSTR(filename_l),
                                          const char *functionName OCCA_F2C_LSTR(functionName_l),
                                          const char *pythonCode   OCCA_F2C_LSTR(pythonCode_l)
@@ -240,64 +295,64 @@ extern "C" {
     return ret;
   }
 
-  occaMemory occaDeviceMalloc_fc(occaDevice device,
+  occaMemory OCCADEVICEMALLOC_FC(occaDevice device,
                                  uintptr_t bytes,
                                  void *source){
     return occaDeviceMalloc(device, bytes, source);
   }
 
-  void occaDeviceFlush_fc(occaDevice device){
+  void OCCADEVICEFLUSH_FC(occaDevice device){
     occaDeviceFlush(device);
   }
-  void occaDeviceFinish_fc(occaDevice device){
+  void OCCADEVICEFINISH_FC(occaDevice device){
     occaDeviceFinish(device);
   }
 
-  occaStream occaDeviceGenStream_fc(occaDevice device){
+  occaStream OCCADEVICEGENSTREAM_FC(occaDevice device){
     return occaDeviceGenStream(device);
   }
-  occaStream occaDeviceGetStream_fc(occaDevice device){
+  occaStream OCCADEVICEGETSTREAM_FC(occaDevice device){
     return occaDeviceGetStream(device);
   }
-  void occaDeviceSetStream_fc(occaDevice device, occaStream stream){
+  void OCCADEVICESETSTREAM_FC(occaDevice device, occaStream stream){
     return occaDeviceSetStream(device, stream);
   }
 
-  occaTag occaDeviceTagStream_fc(occaDevice device){
+  occaTag OCCADEVICETAGSTREAM_FC(occaDevice device){
     return occaDeviceTagStream(device);
   }
-  double occaDeviceTimeBetweenTags_fc(occaDevice device,
+  double OCCADEVICETIMEBETWEENTAGS_FC(occaDevice device,
                                       occaTag startTag, occaTag endTag){
     return occaDeviceTimeBetweenTags(device, startTag, endTag);
   }
 
-  void occaDeviceStreamFree_fc(occaDevice device, occaStream stream){
+  void OCCADEVICESTREAMFREE_FC(occaDevice device, occaStream stream){
     occaDeviceStreamFree(device, stream);
   }
 
-  void occaDeviceFree_fc(occaDevice device){
+  void OCCADEVICEFREE_FC(occaDevice device){
     occaDeviceFree(device);
   }
   //====================================
 
 
   //---[ Kernel ]-----------------------
-  const char* occaKernelMode_fc(occaKernel kernel){ //[-]
+  const char* OCCAKERNELMODE_FC(occaKernel kernel){ //[-]
     occaKernelMode(kernel);
   }
 
-  int occaKernelPreferredDimSize_fc(occaKernel kernel){
+  int OCCAKERNELPREFERREDDIMSIZE_FC(occaKernel kernel){
     return occaKernelPreferredDimSize(kernel);
   }
 
-  void occaKernelSetWorkingDims_fc(occaKernel kernel,
+  void OCCAKERNELSETWORKINGDIMS_FC(occaKernel kernel,
                                    int dims,
                                    occaDim items,
                                    occaDim groups){
     occaKernelSetWorkingDims(kernel, dims, items, groups);
   }
 
-  void occaKernelSetAllWorkingDims_fc(occaKernel kernel,
+  void OCCAKERNELSETALLWORKINGDIMS_FC(occaKernel kernel,
                                       int dims,
                                       uintptr_t itemsX, uintptr_t itemsY, uintptr_t itemsZ,
                                       uintptr_t groupsX, uintptr_t groupsY, uintptr_t groupsZ){
@@ -307,42 +362,42 @@ extern "C" {
                                 groupsX, groupsY, groupsZ);
   }
 
-  double occaKernelTimeTaken_fc(occaKernel kernel){
+  double OCCAKERNELTIMETAKEN_FC(occaKernel kernel){
     return occaKernelTimeTaken(kernel);
   }
 
-  occaArgumentList occaGenArgumentList_fc(){
+  occaArgumentList OCCAGENARGUMENTLIST_FC(){
     return occaGenArgumentList();
   }
 
-  void occaArgumentListClear_fc(occaArgumentList list){
+  void OCCAARGUMENTLISTCLEAR_FC(occaArgumentList list){
     occaArgumentListClear(list);
   }
 
-  void occaArgumentListFree_fc(occaArgumentList list){
+  void OCCAARGUMENTLISTFREE_FC(occaArgumentList list){
     occaArgumentListFree(list);
   }
 
-  void occaArgumentListAddArg_fc(occaArgumentList list,
+  void OCCAARGUMENTLISTADDARG_FC(occaArgumentList list,
                                  int argPos,
                                  void *type){
     occaArgumentListAddArg(list, argPos, type);
   }
 
-  void occaKernelRun__fc(occaKernel kernel,
+  void OCCAKERNELRUN__FC(occaKernel kernel,
                          occaArgumentList list){
     occaKernelRun_(kernel, list);
   }
 
-  void occaKernelFree_fc(occaKernel kernel){
+  void OCCAKERNELFREE_FC(occaKernel kernel){
     occaKernelFree(kernel);
   }
 
-  occaKernelInfo occaGenKernelInfo_fc(){
+  occaKernelInfo OCCAGENKERNELINFO_FC(){
     return occaGenKernelInfo();
   }
 
-  void occaKernelInfoAddDefine_fc(occaKernelInfo info,
+  void OCCAKERNELINFOADDDEFINE_FC(occaKernelInfo info,
                                   const char *macro OCCA_F2C_LSTR(macro_l),
                                   occaType value
                                   OCCA_F2C_RSTR(macro_l)){
@@ -354,18 +409,18 @@ extern "C" {
     OCCA_F2C_FREE_STR(macro, macro_c);
   }
 
-  void occaKernelInfoFree_fc(occaKernelInfo info){
+  void OCCAKERNELINFOFREE_FC(occaKernelInfo info){
     occaKernelInfoFree(info);
   }
   //====================================
 
 
   //---[ Memory ]-----------------------
-  const char* occaMemoryMode_fc(occaMemory memory){ //[-]
+  const char* OCCAMEMORYMODE_FC(occaMemory memory){ //[-]
     occaMemoryMode(memory);
   }
 
-  void occaCopyMemToMem_fc(occaMemory dest, occaMemory src,
+  void OCCACOPYMEMTOMEM_FC(occaMemory dest, occaMemory src,
                            const uintptr_t bytes,
                            const uintptr_t destOffset,
                            const uintptr_t srcOffset){
@@ -373,19 +428,19 @@ extern "C" {
                      bytes, destOffset, srcOffset);
   }
 
-  void occaCopyPtrToMem_fc(occaMemory dest, const void *src,
+  void OCCACOPYPTRTOMEM_FC(occaMemory dest, const void *src,
                            const uintptr_t bytes, const uintptr_t offset){
     occaCopyPtrToMem(dest, src,
                      bytes, offset);
   }
 
-  void occaCopyMemToPtr_fc(void *dest, occaMemory src,
+  void OCCACOPYMEMTOPTR_FC(void *dest, occaMemory src,
                            const uintptr_t bytes, const uintptr_t offset){
     occaCopyMemToPtr(dest, src,
                      bytes, offset);
   }
 
-  void occaAsyncCopyMemToMem_fc(occaMemory dest, occaMemory src,
+  void OCCAASYNCCOPYMEMTOMEM_FC(occaMemory dest, occaMemory src,
                                 const uintptr_t bytes,
                                 const uintptr_t destOffset,
                                 const uintptr_t srcOffset){
@@ -393,23 +448,23 @@ extern "C" {
                           bytes, destOffset, srcOffset);
   }
 
-  void occaAsyncCopyPtrToMem_fc(occaMemory dest, const void *src,
+  void OCCAASYNCCOPYPTRTOMEM_FC(occaMemory dest, const void *src,
                                 const uintptr_t bytes, const uintptr_t offset){
     occaAsyncCopyPtrToMem(dest, src,
                           bytes, offset);
   }
 
-  void occaAsyncCopyMemToPtr_fc(void *dest, occaMemory src,
+  void OCCAASYNCCOPYMEMTOPTR_FC(void *dest, occaMemory src,
                                 const uintptr_t bytes, const uintptr_t offset){
     occaAsyncCopyMemToPtr(dest, src,
                           bytes, offset);
   }
 
-  void occaMemorySwap_fc(occaMemory memoryA, occaMemory memoryB){
+  void OCCAMEMORYSWAP_FC(occaMemory memoryA, occaMemory memoryB){
     occaMemorySwap(memoryA, memoryB);
   }
 
-  void occaMemoryFree_fc(occaMemory memory){
+  void OCCAMEMORYFREE_FC(occaMemory memory){
     occaMemoryFree(memory);
   }
   //====================================
