99d2aff8d70e85a30d61a3c85c660a8b034f8144
Lucas C Wilcox <lucas@swirlee.com>
2014-09-24 11:15:28 -0700

    Update CUDA pow
    Third time is a charm.

--- include/occaKernelDefines.hpp
+++ include/occaKernelDefines.hpp
@@ -2,7 +2,7 @@ namespace occa {
     extern char occaPthreadsDefines[9840];
     extern char occaOpenMPDefines[9990];
     extern char occaOpenCLDefines[9962];
-    extern char occaCUDADefines[12734];
+    extern char occaCUDADefines[12804];
     extern char occaCOIDefines[9901];
     extern char occaCOIMain[37440];
 }
