cfa6f2e2993acc19eab7b44ebace86cd2e87a8e9
dsm5 <dmed256@gmail.com>
2014-11-18 12:09:02 -0600

    [Math] Updated math functions (addex expm1)

--- include/occaKernelDefines.hpp
+++ include/occaKernelDefines.hpp
@@ -3,6 +3,6 @@ namespace occa {
     extern char occaOpenMPDefines[10781];
     extern char occaOpenCLDefines[10053];
     extern char occaCUDADefines[11242];
-    extern char occaCOIDefines[10576];
+    extern char occaCOIDefines[10667];
     extern char occaCOIMain[37440];
 }
