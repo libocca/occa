e0ef463572fbe4986f133400e7e4f01a2c027210
dsm5 <dmed256@gmail.com>
2014-11-22 17:26:11 -0600

    [Web] Added documentation place holders

---
+++ web/documentation/Python.php
@@ -0,0 +1,11 @@
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/header.php'); ?>
+
+<?php absInclude("menu.php"); ?>
+
+<div id="id_body">
+
+  <?php absInclude("/documentation/menu.php") ?>
+
+</div> <!--[ id_body ]-->
+
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/footer.php'); ?>
