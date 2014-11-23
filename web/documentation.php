8d261b409579777afcc5113cb0b39e6383f30d0a
dsm5 <dmed256@gmail.com>
2014-11-23 02:47:08 -0600

    [Web] Minor updates

--- web/documentation.php
+++ web/documentation.php
@@ -8,13 +8,13 @@
   <h2 class="ui dividing header"> Quick Introduction </h2>
 
   <h2 class="ui dividing header">
-    <a href="/documentation/hostAPI.php" class="dsm5 link f_rw"> Host API </a>
+    <a href="/documentation/hostAPI.php" class="dsm5 link"> Host API </a>
   </h2>
 
   We have A B C
 
   <h2 class="ui dividing header">
-    <a href="/documentation/deviceAPI.php" class="dsm5 link f_rw"> Device API </a>
+    <a href="/documentation/deviceAPI.php" class="dsm5 link"> Device API </a>
   </h2>
 
 </div> <!--[ id_body ]-->
