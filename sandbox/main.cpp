e1b37541ba95eeb190f3a1d6faa32e05e3e75827
dsm5 <dmed256@gmail.com>
2014-12-03 03:04:09 -0600

    [Checklist] Update

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -67,11 +67,11 @@ int main(int argc, char **argv){
   //   std::cout << parsedContent << '\n';
   // }
 
-  {
-    occa::parser parser;
-    std::string parsedContent = parser.parseFile("tests/test.cpp");
-    std::cout << parsedContent << '\n';
-  }
+  // {
+  //   occa::parser parser;
+  //   std::string parsedContent = parser.parseFile("tests/test.cpp");
+  //   std::cout << parsedContent << '\n';
+  // }
 
   // {
   //   occa::parser parser;
@@ -115,11 +115,11 @@ int main(int argc, char **argv){
   //   std::cout << parsedContent << '\n';
   // }
 
-  // {
-  //   occa::parser parser;
-  //   std::string parsedContent = parser.parseFile("tests/addVectors.ofl");
-  //   std::cout << parsedContent << '\n';
-  // }
+  {
+    occa::parser parser;
+    std::string parsedContent = parser.parseFile("tests/addVectors.ofl");
+    std::cout << parsedContent << '\n';
+  }
 
   // {
   //   occa::parser parser;
