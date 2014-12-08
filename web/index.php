7b47a209a558cb828c50e006a9aede062f1256c9
dsm5 <dmed256@gmail.com>
2014-12-08 09:33:04 -0600

    [Web] Minor update

--- web/index.php
+++ web/index.php
@@ -35,7 +35,7 @@
 </div>
 
 <div style="width: 100%; background-color: #EAF5F9; padding: 20px 0 20px 0;">
-  <div style="height: 250px; width: 1060px; margin: auto; color: black; margin: 20px auto 20px auto; position: relative; clear: both;">
+  <div style="height: 250px; width: 1060px; margin: 20px auto 20px auto; position: relative; clear: both;">
     <a style="width: 250px; float: left; display: block; position: relative;" href="/downloads.php">
       <div style="margin: auto; width: 80px; color: #6992B5;"><i class="huge download icon"></i></div>
       <p style="text-align: center; color: #6C6C6C; font-size: 18px; font-family: openSansBold; margin: 15px 0 25px 0;">
@@ -46,7 +46,7 @@
       </div>
     </a>
 
-    <div style="height: 1px; width: 20px; float: left; display: block; vertical-align: middle; position: relative"></div>
+    <div style="width: 20px; height: 250px; float: left; display: block; position: relative"></div>
 
     <a style="width: 250px; float: left; display: block; position: relative" href="/getStarted.php">
       <div style="margin: auto; width: 80px; color: #6992B5;"><i class="huge map marker icon"></i></div>
@@ -58,7 +58,7 @@
       </div>
     </a>
 
-    <div style="height: 1px; width: 20px; float: left; display: block; vertical-align: middle; position: relative"></div>
+    <div style="width: 20px; height: 250px; float: left; display: block; position: relative"></div>
 
     <a style="width: 250px; float: left; display: block;" href="/tutorials.php">
       <div style="margin: auto; width: 80px; color: #6992B5;"><i class="huge map icon"></i></div>
@@ -71,7 +71,7 @@
       </div>
     </a>
 
-    <div style="height: 1px; width: 20px; float: left; display: block; vertical-align: middle; position: relative"></div>
+    <div style="width: 20px; height: 250px; float: left; display: block; position: relative"></div>
 
     <a style="width: 250px; float: left; display: block;" href="/documentation.php">
       <div style="margin: auto; width: 80px; color: #6992B5;"><i class="huge settings icon"></i></div>
