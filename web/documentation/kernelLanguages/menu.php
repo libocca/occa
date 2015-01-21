1d64e6a28a8db58cee5bdaff395153813d5e7a87
dsm5 <dmed256@gmail.com>
2015-01-21 07:32:08 -0600

    [Web] Moved to dmed256/OCCA-Website

--- web/documentation/kernelLanguages/menu.php
+++
@@ -1,6 +0,0 @@
-<div class="ui inverted left kernelLanguages menu">
-  <div class="kernelLanguages menu wrapper">
-    <a class="item <?php addSelected('OKL') ?>" href="/documentation/kernelLanguages/OKL.php"> OKL </a>
-    <a class="item <?php addSelected('OFL') ?>" href="/documentation/kernelLanguages/OFL.php"> OFL </a>
-  </div>
-</div>
