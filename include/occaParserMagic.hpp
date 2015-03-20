56e665927389ce9369c7aa4b46cde44ddc6d3a27
dsm5 <dmed256@gmail.com>
2015-03-20 01:12:49 -0500

    [Magic] Added skeleton

--- include/occaParserMagic.hpp
+++ include/occaParserMagic.hpp
@@ -7,6 +7,34 @@ namespace occa {
   namespace parserNS {
     class parserBase;
 
+    class expInfo {
+    public:
+      bool isConstant;
+      expNode exp;
+    };
+
+    class iteratorInfo {
+    public:
+      expInfo bounds[2], stride;
+    };
+
+    class viInfo {
+    public:
+      bool isAnIterator, isConstant, isUseless;
+    };
+
+    typedef std::map<varInfo*, viInfo> viInfoMap_t_;
+    typedef viInfoMap_t_::iterator     viInfoIterator;
+
+    class viInfoMap_t {
+    public:
+      viInfoMap_t_ viMap;
+
+      viInfoMap_t();
+
+      void addVariable(varInfo &var);
+    };
+
     class magician {
     public:
       parserBase &parser;
@@ -14,12 +42,28 @@ namespace occa {
       varUsedMap_t &varUpdateMap;
       varUsedMap_t &varUsedMap;
 
+      std::stack<viInfoMap_t> viInfoMapStack;
+
       magician(parserBase &parser_);
+
+      viInfoMap_t* currentViInfoMap();
+      void pushMapStack();
+      void popMapStack();
 
       static void castMagicOn(parserBase &parser_);
 
       void castMagic();
-      void castMagicOn(statement &kernel);
+      void analyzeFunction(statement &fs);
+      void analyzeStatement(statement &s);
+
+      void analyzeEmbeddedStatements(statement &s);
+
+      void analyzeDeclareStatement(statement &s);
+      void analyzeUpdateStatement(statement &s);
+      bool analyzeForStatement(statement &s);
+      bool analyzeWhileStatement(statement &s);
+      void analyzeIfStatement(statementNode *snStart, statementNode *snEnd);
+      void analyzeSwitchStatement(statement &s);
     };
   };
 };
--- src/occaParserMagic.cpp
+++ src/occaParserMagic.cpp
@@ -2,11 +2,29 @@
 
 namespace occa {
   namespace parserNS {
+    viInfoMap_t::viInfoMap_t(){}
+
+    void viInfoMap_t::addVariable(varInfo &var){
+      viMap[&var];
+    }
+
     magician::magician(parserBase &parser_) :
       parser(parser_),
       globalScope( *(parser_.globalScope) ),
       varUpdateMap(parser_.varUpdateMap),
       varUsedMap(parser_.varUsedMap) {}
+
+    viInfoMap_t* magician::currentViInfoMap(){
+      return &(viInfoMapStack.top());
+    }
+
+    void magician::pushMapStack(){
+      viInfoMapStack.push( viInfoMap_t() );
+    }
+
+    void magician::popMapStack(){
+      viInfoMapStack.pop();
+    }
 
     void magician::castMagicOn(parserBase &parser_){
       magician mickey(parser_);
@@ -20,24 +38,133 @@ namespace occa {
         statement &s = *(sn->value);
 
         if(parser.statementIsAKernel(s))
-          castMagicOn(s);
+          analyzeFunction(s);
 
         sn = sn->right;
       }
     }
 
-    void magician::castMagicOn(statement &kernel){
+    void magician::analyzeFunction(statement &fs){
+      varInfo &func = *(fs.getFunctionVar());
 
-    }
+      pushMapStack();
 
-    void loopCheckStatement(statement *root){
-      if((root == NULL) ||
-         !(root->info & forStatementType)){
+      viInfoMap_t *viMap = currentViInfoMap();
 
-        return;
+      // Place function arguments (if any)
+      if(func.argumentCount){
+        for(int arg = 0; arg < func.argumentCount; ++arg){
+          varInfo &varg = *(func.argumentVarInfos[arg]);
+
+          viMap->addVariable(varg);
+        }
       }
 
-      root->expRoot.print();
+      statementNode *statementPos = fs.statementStart;
+
+      while(statementPos){
+        analyzeStatement( *(statementPos->value) );
+
+        statementPos = statementPos->right;
+      }
+
+      popMapStack();
+    }
+
+    void magician::analyzeStatement(statement &s){
+      bool analyzeEmbedded = true;
+
+      if(s.info & gotoStatementType){
+        printf("Magic Analyzer: Goto statements are not allowed\n");
+      }
+
+      else if(s.info & (typedefStatementType   |
+                        blankStatementType     |
+                        blockStatementType     |
+                        structStatementType    |
+                        functionStatementType  |
+                        functionDefinitionType |
+                        functionPrototypeType)){
+        // Ignore this statement
+      }
+
+      else if(s.info & declareStatementType){
+        analyzeDeclareStatement(s);
+      }
+      else if(s.info & updateStatementType){
+        analyzeUpdateStatement(s);
+      }
+
+      else if(s.info & forStatementType){
+        analyzeEmbedded = analyzeForStatement(s);
+      }
+      else if(s.info & whileStatementType){
+        analyzeEmbedded = analyzeWhileStatement(s);
+      }
+      else if(s.info & doWhileStatementType){
+        analyzeEmbedded = false;
+
+        // do-while guarantees at least one run
+        analyzeEmbeddedStatements(s);
+        analyzeWhileStatement(s);
+      }
+      else if(s.info & ifStatementType){
+        statementNode *snStart = s.getStatementNode();
+        statementNode *snEnd   = snStart->right;
+
+        while(snEnd                                   &&
+              (snEnd->value->info & ifStatementType)  &&
+              (snEnd->value->info != ifStatementType)){
+
+          snEnd = snEnd->right;
+        }
+
+        analyzeEmbedded = false;
+        analyzeIfStatement(snStart, snEnd);
+      }
+      else if(s.info & switchStatementType){
+        analyzeEmbedded = false;
+        analyzeSwitchStatement(s);
+      }
+
+      if(analyzeEmbedded)
+        analyzeEmbeddedStatements(s);
+    }
+
+    void magician::analyzeEmbeddedStatements(statement &s){
+      if(s.statementStart != NULL){
+        pushMapStack();
+
+        statementNode *statementPos = s.statementStart;
+
+        while(statementPos){
+          analyzeStatement( *(statementPos->value) );
+
+          statementPos = statementPos->right;
+        }
+
+        popMapStack();
+      }
+    }
+
+    void magician::analyzeDeclareStatement(statement &s){
+    }
+
+    void magician::analyzeUpdateStatement(statement &s){
+    }
+
+    bool magician::analyzeForStatement(statement &s){
+      return true;
+    }
+
+    bool magician::analyzeWhileStatement(statement &s){
+      return true;
+    }
+
+    void magician::analyzeIfStatement(statementNode *snStart, statementNode *snEnd){
+    }
+
+    void magician::analyzeSwitchStatement(statement &s){
     }
   };
 };
