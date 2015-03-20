fbb7fee9a0e3770736fa6b540d9a2a2e9d3cee24
dsm5 <dmed256@gmail.com>
2015-03-19 21:28:15 -0500

    [Magic] Starting variables over from scratch

--- include/occaParserMagic.hpp
+++ include/occaParserMagic.hpp
@@ -7,56 +7,12 @@ namespace occa {
   namespace parserNS {
     class parserBase;
 
-    class strideInfo {
-    public:
-      bool isConstant;
-      varInfo *varName;
-      expNode stride;
-
-      strideInfo();
-    };
-
-    class accessInfo {
-    public:
-      bool isUseful;
-      std::vector<strideInfo> strides;
-
-      accessInfo();
-
-      void load(expNode &root);
-
-      int dim();
-
-      varInfo& var(const int pos);
-
-      strideInfo& operator [] (const int pos);
-      strideInfo& operator [] (const std::string &varName);
-    };
-
-    class ctInfo {
-    public:
-      bool hasConstValue;
-      typeHolder constValue;
-
-      expNode loopBounds[2];
-      expNode loopStride;
-
-      std::vector<accessInfo> reads;
-      std::vector<accessInfo> writes;
-    };
-
-    typedef std::map<varInfo*, ctInfo> ctMap_t;
-    typedef ctMap_t::iterator          ctMapIterator;
-    typedef ctMap_t::const_iterator    cCtMapIterator;
-
     class magician {
     public:
       parserBase &parser;
       statement &globalScope;
       varUsedMap_t &varUpdateMap;
       varUsedMap_t &varUsedMap;
-
-      ctMap_t ctMap;
 
       magician(parserBase &parser_);
 
