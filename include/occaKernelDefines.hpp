db5258b4d018d8fcdd1891b2ef4a5550aa50bf0e
dsm5 <dmed256@gmail.com>
2015-03-21 12:14:00 -0500

    [Defines] Fixed CUDA atomic operation macros (I think)

--- include/occaKernelDefines.hpp
+++ include/occaKernelDefines.hpp
@@ -2,7 +2,7 @@ namespace occa {
   extern char occaSerialDefines[12831];
   extern char occaOpenMPDefines[14081];
   extern char occaOpenCLDefines[12886];
-  extern char occaCUDADefines[13047];
+  extern char occaCUDADefines[13177];
   extern char occaPthreadsDefines[12825];
   extern char occaCOIDefines[14003];
   extern char occaVectorDefines[328283];
