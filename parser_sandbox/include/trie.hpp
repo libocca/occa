a8ca0dbea7efb596625e5aefcdf24d236e49fdfe
dsm5 <dmed256@gmail.com>
2016-12-30 00:41:54 -0600

    [Refactor] make test works, added trie tests

--- parser_sandbox/include/trie.hpp
+++ parser_sandbox/include/trie.hpp
@@ -4,47 +4,49 @@
 #include <iostream>
 #include <map>
 
-class trieNode_t;
-typedef std::map<char, trieNode_t>    trieNodeMap_t;
-typedef trieNodeMap_t::iterator       trieNodeMapIterator;
-typedef trieNodeMap_t::const_iterator cTrieNodeMapIterator;
+namespace occa {
+  class trieNode_t;
+  typedef std::map<char, trieNode_t>    trieNodeMap_t;
+  typedef trieNodeMap_t::iterator       trieNodeMapIterator;
+  typedef trieNodeMap_t::const_iterator cTrieNodeMapIterator;
 
-class trieNode_t {
-public:
-  bool isLeaf;
-  trieNodeMap_t leaves;
+  class trieNode_t {
+  public:
+    bool isLeaf;
+    trieNodeMap_t leaves;
 
-  trieNode_t();
+    trieNode_t();
 
-  void add(const char *c);
+    void add(const char *c);
 
-  int leafNodes() const;
-  const char* get(const char *c) const;
-};
+    int leafNodes() const;
+    const char* get(const char *c) const;
+  };
 
-class trie_t {
-public:
-  trieNode_t root;
+  class trie_t {
+  public:
+    trieNode_t root;
 
-  int nodeCount;
-  int baseNodeCount;
-  char *chars;
-  int *offsets;
-  int *leafCount;
-  bool *isLeaf;
+    int nodeCount;
+    int baseNodeCount;
+    char *chars;
+    int *offsets;
+    int *leafCount;
+    bool *isLeaf;
 
-  trie_t();
+    trie_t();
 
-  void add(const char *c);
-  void add(const std::string &s);
+    void add(const char *c);
+    void add(const std::string &s);
 
-  void freeze();
-  int freeze(const trieNode_t &node, int offset);
+    void freeze();
+    int freeze(const trieNode_t &node, int offset);
 
-  const char* get(const char *c) const;
-  bool has(const char c) const;
-  bool has(const char *c) const;
+    const char* get(const char *c) const;
+    bool has(const char c) const;
+    bool has(const char *c) const;
 
-  void print() const;
-};
+    void print() const;
+  };
+}
 #endif
