f44b28a012439a79b36a16aba154e7386b2aa1e6
dsm5 <dmed256@gmail.com>
2017-02-12 23:13:16 -0600

    [Copyright] Updated copyright

--- src/operators/cKernelOperators.cpp
+++ src/operators/cKernelOperators.cpp
@@ -1,6 +1,6 @@
 /* The MIT License (MIT)
  * 
- * Copyright (c) 2014-2016 David Medina and Tim Warburton
+ * Copyright (c) 2014-2017 David Medina and Tim Warburton
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
