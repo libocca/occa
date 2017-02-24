636d8b3dd75c408c5fcc2b93f68f8db332afe115
dsm5 <dmed256@gmail.com>
2017-02-24 04:58:12 -0600

    [CI] Fixed parser_sandbox json test

--- parser_sandbox/tests/testJson.cpp
+++ parser_sandbox/tests/testJson.cpp
@@ -25,42 +25,42 @@ void testString() {
   // Normal strings
   j.load("\"A\"");
   OCCA_TEST_COMPARE(occa::json::string_, j.type);
-  OCCA_TEST_COMPARE("A", j.value.string);
+  OCCA_TEST_COMPARE("A", j.value_.string);
   j.load("'A'");
   OCCA_TEST_COMPARE(occa::json::string_, j.type);
-  OCCA_TEST_COMPARE("A", j.value.string);
+  OCCA_TEST_COMPARE("A", j.value_.string);
   j.load("\"A'\"");
   OCCA_TEST_COMPARE(occa::json::string_, j.type);
-  OCCA_TEST_COMPARE("A'", j.value.string);
+  OCCA_TEST_COMPARE("A'", j.value_.string);
   j.load("'A\"'");
   OCCA_TEST_COMPARE(occa::json::string_, j.type);
-  OCCA_TEST_COMPARE("A\"", j.value.string);
+  OCCA_TEST_COMPARE("A\"", j.value_.string);
 
   // Special chars
   j.load("\"\\\"\"");
   OCCA_TEST_COMPARE(occa::json::string_, j.type);
-  OCCA_TEST_COMPARE("\"", j.value.string);
+  OCCA_TEST_COMPARE("\"", j.value_.string);
   j.load("\"\\\\\"");
   OCCA_TEST_COMPARE(occa::json::string_, j.type);
-  OCCA_TEST_COMPARE("\\", j.value.string);
+  OCCA_TEST_COMPARE("\\", j.value_.string);
   j.load("\"\\/\"");
   OCCA_TEST_COMPARE(occa::json::string_, j.type);
-  OCCA_TEST_COMPARE("/", j.value.string);
+  OCCA_TEST_COMPARE("/", j.value_.string);
   j.load("\"\\b\"");
   OCCA_TEST_COMPARE(occa::json::string_, j.type);
-  OCCA_TEST_COMPARE("\b", j.value.string);
+  OCCA_TEST_COMPARE("\b", j.value_.string);
   j.load("\"\\f\"");
   OCCA_TEST_COMPARE(occa::json::string_, j.type);
-  OCCA_TEST_COMPARE("\f", j.value.string);
+  OCCA_TEST_COMPARE("\f", j.value_.string);
   j.load("\"\\n\"");
   OCCA_TEST_COMPARE(occa::json::string_, j.type);
-  OCCA_TEST_COMPARE("\n", j.value.string);
+  OCCA_TEST_COMPARE("\n", j.value_.string);
   j.load("\"\\r\"");
   OCCA_TEST_COMPARE(occa::json::string_, j.type);
-  OCCA_TEST_COMPARE("\r", j.value.string);
+  OCCA_TEST_COMPARE("\r", j.value_.string);
   j.load("\"\\t\"");
   OCCA_TEST_COMPARE(occa::json::string_, j.type);
-  OCCA_TEST_COMPARE("\t", j.value.string);
+  OCCA_TEST_COMPARE("\t", j.value_.string);
 }
 
 void testNumber() {
@@ -68,31 +68,31 @@ void testNumber() {
 
   j.load("-10");
   OCCA_TEST_COMPARE(occa::json::number_, j.type);
-  OCCA_TEST_COMPARE(-10, (int) j.value.number);
+  OCCA_TEST_COMPARE(-10, (int) j.value_.number);
   j.load("10");
   OCCA_TEST_COMPARE(occa::json::number_, j.type);
-  OCCA_TEST_COMPARE(10, (int) j.value.number);
+  OCCA_TEST_COMPARE(10, (int) j.value_.number);
   j.load("0.1");
   OCCA_TEST_COMPARE(occa::json::number_, j.type);
-  OCCA_TEST_COMPARE(0.1, (double) j.value.number);
+  OCCA_TEST_COMPARE(0.1, (double) j.value_.number);
   j.load("0.1e10");
   OCCA_TEST_COMPARE(occa::json::number_, j.type);
-  OCCA_TEST_COMPARE(0.1e10, (double) j.value.number);
+  OCCA_TEST_COMPARE(0.1e10, (double) j.value_.number);
   j.load("0.1E10");
   OCCA_TEST_COMPARE(occa::json::number_, j.type);
-  OCCA_TEST_COMPARE(0.1E10, (double) j.value.number);
+  OCCA_TEST_COMPARE(0.1E10, (double) j.value_.number);
   j.load("0.1e-10");
   OCCA_TEST_COMPARE(occa::json::number_, j.type);
-  OCCA_TEST_COMPARE(0.1e-10, (double) j.value.number);
+  OCCA_TEST_COMPARE(0.1e-10, (double) j.value_.number);
   j.load("0.1E-10");
   OCCA_TEST_COMPARE(occa::json::number_, j.type);
-  OCCA_TEST_COMPARE(0.1E-10, (double) j.value.number);
+  OCCA_TEST_COMPARE(0.1E-10, (double) j.value_.number);
   j.load("0.1e+10");
   OCCA_TEST_COMPARE(occa::json::number_, j.type);
-  OCCA_TEST_COMPARE(0.1e+10, (double) j.value.number);
+  OCCA_TEST_COMPARE(0.1e+10, (double) j.value_.number);
   j.load("0.1E+10");
   OCCA_TEST_COMPARE(occa::json::number_, j.type);
-  OCCA_TEST_COMPARE(0.1E+10, (double) j.value.number);
+  OCCA_TEST_COMPARE(0.1E+10, (double) j.value_.number);
 }
 
 void testObject() {
@@ -100,32 +100,32 @@ void testObject() {
 
  j.load("{\"0\":0, \"1\":1}");
  OCCA_TEST_COMPARE(occa::json::object_, j.type);
- OCCA_TEST_COMPARE(2, (int) j.value.object.size());
- OCCA_TEST_COMPARE(0, (int) j.value.object["0"]);
- OCCA_TEST_COMPARE(1, (int) j.value.object["1"]);
+ OCCA_TEST_COMPARE(2, (int) j.value_.object.size());
+ OCCA_TEST_COMPARE(0, (int) j.value_.object["0"]);
+ OCCA_TEST_COMPARE(1, (int) j.value_.object["1"]);
 
  j.load("{\"0\":0, \"1\":1,}");
  OCCA_TEST_COMPARE(occa::json::object_, j.type);
- OCCA_TEST_COMPARE(2, (int) j.value.object.size());
- OCCA_TEST_COMPARE(occa::json::number_, j.value.object["0"].type);
- OCCA_TEST_COMPARE(occa::json::number_, j.value.object["1"].type);
- OCCA_TEST_COMPARE(0, (int) j.value.object["0"]);
- OCCA_TEST_COMPARE(1, (int) j.value.object["1"]);
+ OCCA_TEST_COMPARE(2, (int) j.value_.object.size());
+ OCCA_TEST_COMPARE(occa::json::number_, j.value_.object["0"].type);
+ OCCA_TEST_COMPARE(occa::json::number_, j.value_.object["1"].type);
+ OCCA_TEST_COMPARE(0, (int) j.value_.object["0"]);
+ OCCA_TEST_COMPARE(1, (int) j.value_.object["1"]);
 
  // Short-hand notation
  j.load("{0:0, 1:1}");
  OCCA_TEST_COMPARE(occa::json::object_, j.type);
- OCCA_TEST_COMPARE(2, (int) j.value.object.size());
- OCCA_TEST_COMPARE(0, (int) j.value.object["0"]);
- OCCA_TEST_COMPARE(1, (int) j.value.object["1"]);
+ OCCA_TEST_COMPARE(2, (int) j.value_.object.size());
+ OCCA_TEST_COMPARE(0, (int) j.value_.object["0"]);
+ OCCA_TEST_COMPARE(1, (int) j.value_.object["1"]);
 
  j.load("{0:0, 1:1,}");
  OCCA_TEST_COMPARE(occa::json::object_, j.type);
- OCCA_TEST_COMPARE(2, (int) j.value.object.size());
- OCCA_TEST_COMPARE(occa::json::number_, j.value.object["0"].type);
- OCCA_TEST_COMPARE(occa::json::number_, j.value.object["1"].type);
- OCCA_TEST_COMPARE(0, (int) j.value.object["0"]);
- OCCA_TEST_COMPARE(1, (int) j.value.object["1"]);
+ OCCA_TEST_COMPARE(2, (int) j.value_.object.size());
+ OCCA_TEST_COMPARE(occa::json::number_, j.value_.object["0"].type);
+ OCCA_TEST_COMPARE(occa::json::number_, j.value_.object["1"].type);
+ OCCA_TEST_COMPARE(0, (int) j.value_.object["0"]);
+ OCCA_TEST_COMPARE(1, (int) j.value_.object["1"]);
 
  // Test path
  j.load("{0: {1: {2: {3: 3}}}}");
@@ -137,21 +137,21 @@ void testArray() {
 
  j.load("[1, 2]");
  OCCA_TEST_COMPARE(occa::json::array_, j.type);
- OCCA_TEST_COMPARE(2, (int) j.value.array.size());
+ OCCA_TEST_COMPARE(2, (int) j.value_.array.size());
 
- OCCA_TEST_COMPARE(occa::json::number_, j.value.array[0].type);
- OCCA_TEST_COMPARE(occa::json::number_, j.value.array[1].type);
- OCCA_TEST_COMPARE(1, (int) j.value.array[0]);
- OCCA_TEST_COMPARE(2, (int) j.value.array[1]);
+ OCCA_TEST_COMPARE(occa::json::number_, j.value_.array[0].type);
+ OCCA_TEST_COMPARE(occa::json::number_, j.value_.array[1].type);
+ OCCA_TEST_COMPARE(1, (int) j.value_.array[0]);
+ OCCA_TEST_COMPARE(2, (int) j.value_.array[1]);
 
  j.load("[1, 2,]");
  OCCA_TEST_COMPARE(occa::json::array_, j.type);
- OCCA_TEST_COMPARE(2, (int) j.value.array.size());
+ OCCA_TEST_COMPARE(2, (int) j.value_.array.size());
 
- OCCA_TEST_COMPARE(occa::json::number_, j.value.array[0].type);
- OCCA_TEST_COMPARE(occa::json::number_, j.value.array[1].type);
- OCCA_TEST_COMPARE(1, (int) j.value.array[0]);
- OCCA_TEST_COMPARE(2, (int) j.value.array[1]);
+ OCCA_TEST_COMPARE(occa::json::number_, j.value_.array[0].type);
+ OCCA_TEST_COMPARE(occa::json::number_, j.value_.array[1].type);
+ OCCA_TEST_COMPARE(1, (int) j.value_.array[0]);
+ OCCA_TEST_COMPARE(2, (int) j.value_.array[1]);
 }
 
 void testKeywords() {
@@ -159,10 +159,10 @@ void testKeywords() {
 
  j.load("true");
  OCCA_TEST_COMPARE(occa::json::boolean_, j.type);
- OCCA_TEST_COMPARE(true, j.value.boolean);
+ OCCA_TEST_COMPARE(true, j.value_.boolean);
  j.load("false");
  OCCA_TEST_COMPARE(occa::json::boolean_, j.type);
- OCCA_TEST_COMPARE(false, j.value.boolean);
+ OCCA_TEST_COMPARE(false, j.value_.boolean);
  j.load("null");
  OCCA_TEST_COMPARE(occa::json::null_, j.type);
 }
