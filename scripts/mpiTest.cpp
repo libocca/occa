70b27069cfc950ab30b116568adc6d125258ad19
dmed256 <dmed256@gmail.com>
2017-04-14 15:59:25 -0500

    [MPI] Added OCCA_MPI_ENABLED flag

--- scripts/mpiTest.cpp
+++ scripts/mpiTest.cpp
@@ -20,35 +20,10 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  */
 
-#ifndef OCCA_DEFINES_COMPILEDDEFINES_HEADER
-#define OCCA_DEFINES_COMPILEDDEFINES_HEADER
+#include <mpi.h>
 
-#ifndef OCCA_LINUX_OS
-#  define OCCA_LINUX_OS 1
-#endif
-
-#ifndef OCCA_OSX_OS
-#  define OCCA_OSX_OS 2
-#endif
-
-#ifndef OCCA_WINDOWS_OS
-#  define OCCA_WINDOWS_OS 4
-#endif
-
-#ifndef OCCA_WINUX_OS
-#  define OCCA_WINUX_OS (OCCA_LINUX_OS | OCCA_WINDOWS_OS)
-#endif
-
-#define OCCA_OS             @@OCCA_OS@@
-#define OCCA_USING_VS       @@OCCA_USING_VS@@
-#define OCCA_COMPILED_DIR   @@OCCA_COMPILED_DIR@@
-
-#define OCCA_DEBUG_ENABLED  @@OCCA_DEBUG_ENABLED@@
-#define OCCA_CHECK_ENABLED  @@OCCA_CHECK_ENABLED@@
-
-#define OCCA_MPI_ENABLED    @@OCCA_MPI_ENABLED@@
-#define OCCA_OPENMP_ENABLED @@OCCA_OPENMP_ENABLED@@
-#define OCCA_OPENCL_ENABLED @@OCCA_OPENCL_ENABLED@@
-#define OCCA_CUDA_ENABLED   @@OCCA_CUDA_ENABLED@@
-
-#endif
+int main(int argc, char **argv) {
+  MPI_Init(&argc, &argv);
+  MPI_Finalize();
+  return 0;
+}
