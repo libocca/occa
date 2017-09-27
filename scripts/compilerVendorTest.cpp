b4e2cb067bd9560b137f41b044dd8937e2b4ac9a
dmed256 <dmed256@gmail.com>
2017-09-27 17:20:51 -0500

    [Compiler] Styling fixes

--- scripts/compilerVendorTest.cpp
+++ scripts/compilerVendorTest.cpp
@@ -1,17 +1,17 @@
 /* The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2014-2017 David Medina and Tim Warburton
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -33,9 +33,9 @@
 
 int main(int argc, char **argv) {
 
-#if defined(__xlc__) || defined(__xlC__) \
+#if  defined(__xlc__)  || defined(__xlC__)    \
   || defined(__IBMC__) || defined(__IBMCPP__) \
-  || defined( __ibmxl__)
+  || defined(__ibmxl__)
   return OCCA_IBM_VENDOR;
 
 #elif defined(__ICC) || defined(__INTEL_COMPILER)
