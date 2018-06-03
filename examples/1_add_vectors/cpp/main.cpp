da47ab6c66a71909d4a771ece10e2c8aace07db8
dmed256 <dmed256@gmail.com>
2018-06-03 16:23:34 -0500

    [Example] Default should be Serial mode

--- examples/1_add_vectors/cpp/main.cpp
+++ examples/1_add_vectors/cpp/main.cpp
@@ -48,8 +48,7 @@ int main(int argc, char **argv) {
   occa::memory o_a, o_b, o_ab;
 
   //---[ Device setup with string flags ]-------------------
-  device.setup("mode: 'OpenMP'");
-  // device.setup("mode: 'Serial'");
+  device.setup("mode: 'Serial'");
 
   // device.setup("mode     : 'OpenMP', "
   //              "schedule : 'compact', "
