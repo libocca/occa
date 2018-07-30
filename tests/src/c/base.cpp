f2048a71943cfe5a249f69284e043e46984c04b6
dmed256 <dmed256@gmail.com>
2018-07-29 23:08:56 -0500

    [CI] Updated c/base tests

--- tests/src/c/base.cpp
+++ tests/src/c/base.cpp
@@ -105,7 +105,7 @@ void testMemory() {
 
   ptr = occaUMalloc(bytes,
                     NULL,
-                    occaDefault);
+                    props);
   occaFreeUvaPtr(ptr);
 
   occaFree(props);
