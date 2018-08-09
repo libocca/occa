7279da0d01436208d0a13e4b61a90e087b5d81c8
dmed256 <dmed256@gmail.com>
2018-08-09 11:25:30 -0600

    [Version] Updated to 1.0.7 for next release

--- include/occa/defines.hpp
+++ include/occa/defines.hpp
@@ -277,15 +277,15 @@
 //---[ OCCA ]---------------------------
 #define OCCA_MAJOR_VERSION 1
 #define OCCA_MINOR_VERSION 0
-#define OCCA_PATCH_VERSION 6
-#define OCCA_VERSION       10006
-#define OCCA_VERSION_STR   "1.0.6"
+#define OCCA_PATCH_VERSION 7
+#define OCCA_VERSION       10007
+#define OCCA_VERSION_STR   "1.0.7"
 
 #define OKL_MAJOR_VERSION 1
 #define OKL_MINOR_VERSION 0
-#define OKL_PATCH_VERSION 6
-#define OKL_VERSION       10006
-#define OKL_VERSION_STR   "1.0.6"
+#define OKL_PATCH_VERSION 7
+#define OKL_VERSION       10007
+#define OKL_VERSION_STR   "1.0.7"
 
 #define OCCA_MAX_ARGS 50
 //======================================
