d03a34e82c4cab792ec70daca4bde45ecfa59df5
dmed256 <dmed256@gmail.com>
2018-09-06 21:29:02 -0500

    [Test] Split core/tools/types tests into directories

--- tests/src/io/utils.cpp
+++ tests/src/io/utils.cpp
@@ -157,7 +157,7 @@ void testDirMethods() {
             0);
   dirs = occa::io::directories(testDir);
   ASSERT_EQ((int) dirs.size(),
-            4);
+            6);
 
   ASSERT_IN(testDir + "c/", dirs);
   ASSERT_IN(testDir + "io/", dirs);
