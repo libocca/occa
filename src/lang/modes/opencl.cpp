27a7420ae544600f5c39c9fcae385307e193d3eb
dmed256 <dmed256@gmail.com>
2019-08-05 16:08:30 -0500

    [OpenCL] Typedef'd const pointers shouldn't get auto-converted to __constant

--- src/lang/modes/opencl.cpp
+++ src/lang/modes/opencl.cpp
@@ -108,7 +108,7 @@ namespace occa {
           const int declCount = declSmnt.declarations.size();
           for (int di = 0; di < declCount; ++di) {
             variable_t &var = *(declSmnt.declarations[di].variable);
-            if (var.has(const_)) {
+            if (var.has(const_) && !var.has(typedef_)) {
               var -= const_;
               var.add(0, constant);
             }
