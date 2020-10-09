71117032b9778ca7cdf597918d080ae44be572d8
Cedric Andreolli <candreol@localhost.localdomain>
2020-10-09 11:15:34 -0400

    Patching computation of local and global ndranges

--- examples/cpp/01_add_vectors/main.cpp
+++ examples/cpp/01_add_vectors/main.cpp
@@ -75,7 +75,7 @@ int main(int argc, const char **argv) {
   // Compile the kernel at run-time
   occa::kernel addVectors = device.buildKernel("addVectors.okl",
                                   "addVectors", kernelProps);
-  addVectors.setRunDims(entries, 1);
+  addVectors.setRunDims(entries, 4);
 
   // Copy memory to the device
   o_a.copyFrom(a);
--- src/modes/dpcpp/kernel.cpp
+++ src/modes/dpcpp/kernel.cpp
@@ -67,7 +67,10 @@ namespace occa {
       // Setup kernel dimensions
       occa::dim fullDims = (outerDims * innerDims);
       ::sycl::queue *q = getCommandQueue();
-      auto global_range = ::sycl::range<3>(outerDims.x, outerDims.y, outerDims.z);
+      int odimx = (outerDims.x % innerDims.x == 0)? outerDims.x : innerDims.x * ((outerDims.x / innerDims.x)+1);
+      int odimy = (outerDims.y % innerDims.y == 0)? outerDims.y : innerDims.y * ((outerDims.y / innerDims.y)+1);
+      int odimz = (outerDims.z % innerDims.z == 0)? outerDims.z : innerDims.z * ((outerDims.z / innerDims.z)+1);
+      auto global_range = ::sycl::range<3>(odimx, odimy, odimz);
       auto local_range  = ::sycl::range<3>(innerDims.x, innerDims.y, innerDims.z);
       std::vector<void*> args;
       ::sycl::nd_range<3> ndrange = ::sycl::nd_range<3>(global_range, local_range);
