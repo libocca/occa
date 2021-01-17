204fd1ad06dc6cad32debc79738a351b352453a9
dmed256 <dmed256@gmail.com>
2021-01-17 00:13:39 -0500

    [Experimental] Adds occa::array

---
+++ include/occa/experimental/functional.hpp
@@ -0,0 +1,9 @@
+#ifndef OCCA_EXPERIMENTAL_FUNCTIONAL_HEADER
+#define OCCA_EXPERIMENTAL_FUNCTIONAL_HEADER
+
+#include <occa/experimental/functional/array.hpp>
+#include <occa/experimental/functional/function.hpp>
+#include <occa/experimental/functional/scope.hpp>
+#include <occa/experimental/functional/utils.hpp>
+
+#endif
