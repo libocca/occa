0aa918346329ebab1481e90486b7d903b60ba655
Kris Rowe <kris.rowe@anl.gov>
2021-09-08 22:24:26 +0000

    Undo source auto formatting.

--- src/occa/internal/lang/expr/exprNode.cpp
+++ src/occa/internal/lang/expr/exprNode.cpp
@@ -1,123 +1,112 @@
 #include <occa/internal/utils/lex.hpp>
 #include <occa/internal/lang/expr/exprNode.hpp>
 
-namespace occa
-{
-  namespace lang
-  {
-    namespace exprNodeType
-    {
-      const udim_t empty = (((uint64_t)1) << 0);
-      const udim_t primitive = (((uint64_t)1) << 1);
-      const udim_t char_ = (((uint64_t)1) << 2);
-      const udim_t string = (((uint64_t)1) << 3);
-      const udim_t identifier = (((uint64_t)1) << 4);
-      const udim_t type = (((uint64_t)1) << 5);
-      const udim_t vartype = (((uint64_t)1) << 6);
-      const udim_t variable = (((uint64_t)1) << 7);
-      const udim_t function = (((uint64_t)1) << 8);
+namespace occa {
+  namespace lang {
+    namespace exprNodeType {
+      const udim_t empty             = (((uint64_t) 1) << 0);
+      const udim_t primitive         = (((uint64_t) 1) << 1);
+      const udim_t char_             = (((uint64_t) 1) << 2);
+      const udim_t string            = (((uint64_t) 1) << 3);
+      const udim_t identifier        = (((uint64_t) 1) << 4);
+      const udim_t type              = (((uint64_t) 1) << 5);
+      const udim_t vartype           = (((uint64_t) 1) << 6);
+      const udim_t variable          = (((uint64_t) 1) << 7);
+      const udim_t function          = (((uint64_t) 1) << 8);
 
-      const udim_t value = (primitive |
-                            type |
-                            vartype |
-                            variable |
-                            function);
+      const udim_t value             = (primitive |
+                                        type      |
+                                        vartype   |
+                                        variable  |
+                                        function);
 
-      const udim_t rawOp = (((uint64_t)1) << 9);
-      const udim_t leftUnary = (((uint64_t)1) << 10);
-      const udim_t rightUnary = (((uint64_t)1) << 11);
-      const udim_t binary = (((uint64_t)1) << 12);
-      const udim_t ternary = (((uint64_t)1) << 13);
-      const udim_t op = (leftUnary |
-                         rightUnary |
-                         binary |
-                         ternary);
+      const udim_t rawOp             = (((uint64_t) 1) << 9);
+      const udim_t leftUnary         = (((uint64_t) 1) << 10);
+      const udim_t rightUnary        = (((uint64_t) 1) << 11);
+      const udim_t binary            = (((uint64_t) 1) << 12);
+      const udim_t ternary           = (((uint64_t) 1) << 13);
+      const udim_t op                = (leftUnary  |
+                                        rightUnary |
+                                        binary     |
+                                        ternary);
 
-      const udim_t pair = (((uint64_t)1) << 14);
+      const udim_t pair              = (((uint64_t) 1) << 14);
 
-      const udim_t subscript = (((uint64_t)1) << 15);
-      const udim_t call = (((uint64_t)1) << 16);
+      const udim_t subscript         = (((uint64_t) 1) << 15);
+      const udim_t call              = (((uint64_t) 1) << 16);
 
-      const udim_t sizeof_ = (((uint64_t)1) << 17);
-      const udim_t sizeof_pack_ = (((uint64_t)1) << 18);
-      const udim_t new_ = (((uint64_t)1) << 19);
-      const udim_t delete_ = (((uint64_t)1) << 20);
-      const udim_t throw_ = (((uint64_t)1) << 21);
+      const udim_t sizeof_           = (((uint64_t) 1) << 17);
+      const udim_t sizeof_pack_      = (((uint64_t) 1) << 18);
+      const udim_t new_              = (((uint64_t) 1) << 19);
+      const udim_t delete_           = (((uint64_t) 1) << 20);
+      const udim_t throw_            = (((uint64_t) 1) << 21);
 
-      const udim_t typeid_ = (((uint64_t)1) << 22);
-      const udim_t noexcept_ = (((uint64_t)1) << 23);
-      const udim_t alignof_ = (((uint64_t)1) << 24);
+      const udim_t typeid_           = (((uint64_t) 1) << 22);
+      const udim_t noexcept_         = (((uint64_t) 1) << 23);
+      const udim_t alignof_          = (((uint64_t) 1) << 24);
 
-      const udim_t const_cast_ = (((uint64_t)1) << 25);
-      const udim_t dynamic_cast_ = (((uint64_t)1) << 26);
-      const udim_t static_cast_ = (((uint64_t)1) << 27);
-      const udim_t reinterpret_cast_ = (((uint64_t)1) << 28);
+      const udim_t const_cast_       = (((uint64_t) 1) << 25);
+      const udim_t dynamic_cast_     = (((uint64_t) 1) << 26);
+      const udim_t static_cast_      = (((uint64_t) 1) << 27);
+      const udim_t reinterpret_cast_ = (((uint64_t) 1) << 28);
 
-      const udim_t funcCast = (((uint64_t)1) << 29);
-      const udim_t parenCast = (((uint64_t)1) << 30);
-      const udim_t constCast = (((uint64_t)1) << 31);
-      const udim_t staticCast = (((uint64_t)1) << 32);
-      const udim_t reinterpretCast = (((uint64_t)1) << 33);
-      const udim_t dynamicCast = (((uint64_t)1) << 34);
+      const udim_t funcCast          = (((uint64_t) 1) << 29);
+      const udim_t parenCast         = (((uint64_t) 1) << 30);
+      const udim_t constCast         = (((uint64_t) 1) << 31);
+      const udim_t staticCast        = (((uint64_t) 1) << 32);
+      const udim_t reinterpretCast   = (((uint64_t) 1) << 33);
+      const udim_t dynamicCast       = (((uint64_t) 1) << 34);
 
-      const udim_t parentheses = (((uint64_t)1) << 35);
-      const udim_t tuple = (((uint64_t)1) << 36);
-      const udim_t cudaCall = (((uint64_t)1) << 37);
-      const udim_t lambda = (((uint64_t)1) << 38);
-      const udim_t dpcppLocalMemory = (((uint64_t)1) << 39);
-      const udim_t dpcppAtomic = (((uint64_t)1) << 40);
+      const udim_t parentheses       = (((uint64_t) 1) << 35);
+      const udim_t tuple             = (((uint64_t) 1) << 36);
+      const udim_t cudaCall          = (((uint64_t) 1) << 37);
+      
+      const udim_t lambda            = (((uint64_t)1) << 38);
+      const udim_t dpcppLocalMemory  = (((uint64_t)1) << 39);
+      const udim_t dpcppAtomic       = (((uint64_t)1) << 40);
     }
 
-    exprNode::exprNode(token_t *token_) : token(token_t::clone(token_)) {}
+    exprNode::exprNode(token_t *token_) :
+      token(token_t::clone(token_)) {}
 
-    exprNode::~exprNode()
-    {
+    exprNode::~exprNode() {
       delete token;
     }
 
-    exprNode *exprNode::clone(exprNode *expr)
-    {
-      if (!expr)
-      {
+    exprNode* exprNode::clone(exprNode *expr) {
+      if (!expr) {
         return NULL;
       }
       return expr->clone();
     }
 
-    bool exprNode::canEvaluate() const
-    {
+    bool exprNode::canEvaluate() const {
       return false;
     }
 
-    primitive exprNode::evaluate() const
-    {
+    primitive exprNode::evaluate() const {
       return primitive();
     }
 
-    exprNode *exprNode::startNode()
-    {
+    exprNode* exprNode::startNode() {
       return this;
     }
 
-    exprNode *exprNode::endNode()
-    {
+    exprNode* exprNode::endNode() {
       return this;
     }
 
-    exprNodeVector exprNode::getNestedChildren()
-    {
+    exprNodeVector exprNode::getNestedChildren() {
       exprNodeVector children;
       pushNestedChildNodes(children);
       return children;
     }
 
-    void exprNode::pushNestedChildNodes(exprNodeVector &children)
-    {
+    void exprNode::pushNestedChildNodes(exprNodeVector &children) {
       exprNodeVector directChildren;
       pushChildNodes(directChildren);
 
-      for (exprNode *child : directChildren)
-      {
+      for (exprNode *child : directChildren) {
         child->pushNestedChildNodes(children);
         children.push_back(child);
       }
@@ -125,19 +114,15 @@ namespace occa
 
     void exprNode::pushChildNodes(exprNodeVector &children) {}
 
-    bool exprNode::replaceExprNode(exprNode *currentNode, exprNode *newNode)
-    {
+    bool exprNode::replaceExprNode(exprNode *currentNode, exprNode *newNode) {
       exprNodeVector directChildren;
       pushChildNodes(directChildren);
 
-      for (exprNode *child : directChildren)
-      {
-        if (child == currentNode)
-        {
+      for (exprNode *child : directChildren) {
+        if (child == currentNode) {
           return safeReplaceExprNode(currentNode, newNode);
         }
-        if (child->replaceExprNode(currentNode, newNode))
-        {
+        if (child->replaceExprNode(currentNode, newNode)) {
           return true;
         }
       }
@@ -145,87 +130,72 @@ namespace occa
       return false;
     }
 
-    bool exprNode::safeReplaceExprNode(exprNode *currentNode, exprNode *newNode)
-    {
+    bool exprNode::safeReplaceExprNode(exprNode *currentNode, exprNode *newNode) {
       // Shouldn't get here
       return false;
     }
 
-    bool exprNode::hasAttribute(const std::string &attr) const
-    {
+    bool exprNode::hasAttribute(const std::string &attr) const {
       return false;
     }
 
-    variable_t *exprNode::getVariable()
-    {
+    variable_t* exprNode::getVariable() {
       return NULL;
     }
 
-    exprNode *exprNode::wrapInParentheses()
-    {
+    exprNode* exprNode::wrapInParentheses() {
       return clone();
     }
 
-    std::string exprNode::toString() const
-    {
+    std::string exprNode::toString() const {
       printer pout;
       pout << (*this);
       return pout.str();
     }
 
-    void exprNode::printWarning(const std::string &message) const
-    {
+    void exprNode::printWarning(const std::string &message) const {
       token->printWarning(message);
     }
 
-    void exprNode::printError(const std::string &message) const
-    {
+    void exprNode::printError(const std::string &message) const {
       token->printError(message);
     }
 
-    void exprNode::debugPrint() const
-    {
+    void exprNode::debugPrint() const {
       debugPrint("");
       io::stderr << '\n';
     }
 
-    void exprNode::childDebugPrint(const std::string &prefix) const
-    {
+    void exprNode::childDebugPrint(const std::string &prefix) const {
       debugPrint(prefix + "|   ");
     }
 
-    io::output &operator<<(io::output &out,
-                           const exprNode &node)
-    {
+    io::output& operator << (io::output &out,
+                             const exprNode &node) {
       printer pout(out);
       node.print(pout);
       return out;
     }
 
-    printer &operator<<(printer &pout,
-                        const exprNode &node)
-    {
+    printer& operator << (printer &pout,
+                          const exprNode &node) {
       node.print(pout);
       return pout;
     }
 
     void cloneExprNodeVector(exprNodeVector &dest,
-                             const exprNodeVector &src)
-    {
-      const int nodes = (int)src.size();
+                             const exprNodeVector &src) {
+      const int nodes = (int) src.size();
       dest.clear();
       dest.reserve(nodes);
-      for (int i = 0; i < nodes; ++i)
-      {
+      for (int i = 0; i < nodes; ++i) {
         dest.push_back(src[i]->clone());
       }
     }
 
-    void freeExprNodeVector(exprNodeVector &vec)
-    {
-      const int nodes = (int)vec.size();
-      for (int i = 0; i < nodes; ++i)
-      {
+    void freeExprNodeVector(exprNodeVector &vec) {
+      const int nodes = (int) vec.size();
+      for (int i = 0; i < nodes; ++i) {
         delete vec[i];
       }
       vec.clear();
--- src/occa/internal/lang/modes/withLauncher.cpp
+++ src/occa/internal/lang/modes/withLauncher.cpp
@@ -19,13 +19,11 @@ namespace occa {
       }
 
       //---[ Public ]-------------------
-      bool withLauncher::succeeded() const
-      {
+      bool withLauncher::succeeded() const {
         return (success && launcherParser.success);
       }
 
-      void withLauncher::writeLauncherSourceToFile(const std::string &filename) const
-      {
+      void withLauncher::writeLauncherSourceToFile(const std::string &filename) const {
         launcherParser.writeToFile(filename);
       }
       //================================
@@ -43,24 +41,19 @@ namespace occa {
         if (!success) return;
         setOklLoopIndices();
 
-        if (!success)
-          return;
+        if (!success) return;
         setupLauncherParser();
 
-        if (!success)
-          return;
+        if (!success) return;
         beforeKernelSplit();
 
-        if (!success)
-          return;
+        if (!success) return;
         splitKernels();
 
-        if (!success)
-          return;
+        if (!success) return;
         setupKernels();
 
-        if (!success)
-          return;
+        if (!success) return;
         afterKernelSplit();
       }
 
@@ -81,10 +74,9 @@ namespace occa {
           .forEachKernelStatement(okl::setOklLoopIndices);
       }
 
-      void withLauncher::setupLauncherParser()
-      {
+      void withLauncher::setupLauncherParser() {
         // Clone source
-        blockStatement &rootClone = (blockStatement &)root.clone();
+        blockStatement &rootClone = (blockStatement&) root.clone();
 
         launcherParser.root.swap(rootClone);
         delete &rootClone;
@@ -137,13 +129,11 @@ namespace occa {
         kernelSmnt.updateVariableReferences();
       }
 
-      bool withLauncher::isOuterMostOuterLoop(forStatement &forSmnt)
-      {
+      bool withLauncher::isOuterMostOuterLoop(forStatement &forSmnt) {
         return isOuterMostOklLoop(forSmnt, "outer");
       }
 
-      bool withLauncher::isOuterMostInnerLoop(forStatement &forSmnt)
-      {
+      bool withLauncher::isOuterMostInnerLoop(forStatement &forSmnt) {
         return isOuterMostOklLoop(forSmnt, "inner");
       }
 
@@ -180,11 +170,9 @@ namespace occa {
 
       void withLauncher::setKernelLaunch(functionDeclStatement &kernelSmnt,
                                          forStatement &forSmnt,
-                                         const int kernelIndex)
-      {
+                                         const int kernelIndex) {
         forStatement *innerSmnt = getInnerMostInnerLoop(forSmnt);
-        if (!innerSmnt)
-        {
+        if (!innerSmnt) {
           success = false;
           forSmnt.printError("No [@inner] for-loop found");
           return;
@@ -205,8 +193,7 @@ namespace occa {
         for (int i = 0; i < pathCount; ++i) {
           forStatement &pathSmnt = *((forStatement*) path[i]);
           oklForStatement oklForSmnt(pathSmnt);
-          if (!oklForSmnt.isValid())
-          {
+          if (!oklForSmnt.isValid()) {
             success = false;
             return;
           }
@@ -218,9 +205,8 @@ namespace occa {
         const int innerDims = innerCount;
 
         // TODO 1.1: Properly fix this
-        for (int i = 0; i < pathCount; ++i)
-        {
-          forStatement &pathSmnt = *((forStatement *)path[i]);
+        for (int i = 0; i < pathCount; ++i) {
+          forStatement &pathSmnt = *((forStatement*) path[i]);
           oklForStatement oklForSmnt(pathSmnt);
 
           launchBlock.add(pathSmnt.init->clone(&launchBlock));
@@ -230,18 +216,20 @@ namespace occa {
           innerCount -= !isOuter;
 
           const int index = (isOuter
-                                 ? outerCount
-                                 : innerCount);
+                             ? outerCount
+                             : innerCount);
           token_t *source = pathSmnt.source;
           const std::string &name = (isOuter
                                      ? "outer"
                                      : "inner");
 
           launchBlock.add(
-              *(new expressionStatement(
-                  &launchBlock,
-                  setDim(source, name, index,
-                         oklForSmnt.getIterationCount()))));
+            *(new expressionStatement(
+                &launchBlock,
+                setDim(source, name, index,
+                       oklForSmnt.getIterationCount())
+              ))
+          );
         }
 
         // Kernel launch
@@ -295,9 +283,8 @@ namespace occa {
         type_t &memoryType = getMemoryModeType();
 
         // Convert pointer arguments to modeMemory_t
-        int argCount = (int)func.args.size();
-        for (int i = 0; i < argCount; ++i)
-        {
+        int argCount = (int) func.args.size();
+        for (int i = 0; i < argCount; ++i) {
           variable_t &arg = *(func.args[i]);
           arg.vartype = arg.vartype.flatten();
           if (arg.vartype.isPointerType()) {
@@ -330,14 +317,12 @@ namespace occa {
         );
       }
 
-      int withLauncher::getInnerLoopLevel(forStatement &forSmnt)
-      {
+      int withLauncher::getInnerLoopLevel(forStatement &forSmnt) {
         statement_t *smnt = forSmnt.up;
         int level = 0;
-        while (smnt)
-        {
-          if ((smnt->type() & statementType::for_) && smnt->hasAttribute("inner"))
-          {
+        while (smnt) {
+          if ((smnt->type() & statementType::for_)
+              && smnt->hasAttribute("inner")) {
             ++level;
           }
           smnt = smnt->up;
@@ -364,18 +349,19 @@ namespace occa {
         return innerMostInnerLoop;
       }
 
-      exprNode &withLauncher::setDim(token_t *source,
+      exprNode& withLauncher::setDim(token_t *source,
                                      const std::string &name,
                                      const int index,
-                                     exprNode *value)
-      {
+                                     exprNode *value) {
         identifierNode var(source, name);
         primitiveNode idx(source, index);
         subscriptNode access(source, var, idx);
-        exprNode &assign = (*(new binaryOpNode(source,
-                                               op::assign,
-                                               access,
-                                               *value)));
+        exprNode &assign = (
+          *(new binaryOpNode(source,
+                             op::assign,
+                             access,
+                             *value))
+        );
         delete value;
         return assign;
       }
@@ -407,8 +393,7 @@ namespace occa {
           });
 
         int smntIndex = kernelSmnt.childIndex();
-        for (int i = (kernelIndex - 1); i >= 0; --i)
-        {
+        for (int i = (kernelIndex - 1); i >= 0; --i) {
           root.add(*(newKernelSmnts[i]),
                    smntIndex);
         }
@@ -423,10 +408,9 @@ namespace occa {
         // delete &kernelSmnt;
       }
 
-      statement_t *withLauncher::extractLoopAsKernel(functionDeclStatement &kernelSmnt,
+      statement_t* withLauncher::extractLoopAsKernel(functionDeclStatement &kernelSmnt,
                                                      forStatement &forSmnt,
-                                                     const int kernelIndex)
-      {
+                                                     const int kernelIndex) {
 
         function_t &oldFunction = kernelSmnt.function();
         function_t &newFunction = (function_t&) oldFunction.clone();
@@ -435,12 +419,13 @@ namespace occa {
         newFunction.source->value = ss.str();
 
         functionDeclStatement &newKernelSmnt = *(
-            new functionDeclStatement(&root,
-                                      newFunction));
+          new functionDeclStatement(&root,
+                                    newFunction)
+        );
         newKernelSmnt.attributes = kernelSmnt.attributes;
 
         // Clone for-loop and replace argument variables
-        forStatement &newForSmnt = (forStatement &)forSmnt.clone();
+        forStatement &newForSmnt = (forStatement&) forSmnt.clone();
         newKernelSmnt.set(newForSmnt);
 
         const int argc = (int) newFunction.args.size();
@@ -506,21 +491,24 @@ namespace occa {
           return;
         }
 
-        statement_t &barrierSmnt = (*(new emptyStatement(forSmnt.up,
-                                                         forSmnt.source)));
+        statement_t &barrierSmnt = (
+          *(new emptyStatement(forSmnt.up,
+                               forSmnt.source))
+        );
 
         identifierToken barrierToken(forSmnt.source->origin,
                                      "barrier");
 
-        barrierSmnt.attributes["barrier"] = (attributeToken_t(*(getAttribute("barrier")),
-                                                              barrierToken));
+        barrierSmnt.attributes["barrier"] = (
+          attributeToken_t(*(getAttribute("barrier")),
+                           barrierToken)
+        );
 
         forSmnt.up->addAfter(forSmnt,
                              barrierSmnt);
       }
 
-      bool withLauncher::writesToShared(exprNode &expr)
-      {
+      bool withLauncher::writesToShared(exprNode &expr) {
         // TODO 1.1: Propertly check read<-->write or write<-->write ordering
         // exprOpNode &opNode = (exprOpNode&) expr;
         // if (!(opNode.opType() & (operatorType::increment |
@@ -535,18 +523,14 @@ namespace occa {
                 var->hasAttribute("shared"));
       }
 
-      void withLauncher::replaceOccaFor(forStatement &forSmnt)
-      {
+      void withLauncher::replaceOccaFor(forStatement &forSmnt) {
         oklForStatement oklForSmnt(forSmnt);
 
         std::string iteratorName;
         const int loopIndex = oklForSmnt.oklLoopIndex();
-        if (oklForSmnt.isOuterLoop())
-        {
+        if (oklForSmnt.isOuterLoop()) {
           iteratorName = getOuterIterator(loopIndex);
-        }
-        else
-        {
+        } else {
           iteratorName = getInnerIterator(loopIndex);
         }
 
@@ -569,18 +553,19 @@ namespace occa {
         blockSmnt.up->children[childIndex] = &blockSmnt;
 
         // Add declaration before block
-        declarationStatement &declSmnt = (*(new declarationStatement(blockSmnt.up,
-                                                                     forSmnt.source)));
+        declarationStatement &declSmnt = (
+          *(new declarationStatement(blockSmnt.up,
+                                     forSmnt.source))
+        );
         declSmnt.declarations.push_back(decl);
 
         blockSmnt.addFirst(declSmnt);
         delete &forSmnt;
       }
 
-      bool withLauncher::usesBarriers()
-      {
+      bool withLauncher::usesBarriers() {
         return true;
       }
-    } // namespace okl
-  }   // namespace lang
-} // namespace occa
+    }
+  }
+}
