831e48f3e0595deafe54cf8d07153fbdcd31dfe8
AljenU <74405789+AljenU@users.noreply.github.com>
2021-11-15 19:16:29 +0100

    Package config with Find module wrappers (#533)
    * CmakeLists.txt: add include dir to install interface
    
    When building against libocca, projects will need the include files.
    
    * Cmake dependency checking: consistent use of find_package
    
    Why: In order to prepare for adding a package config for occa itself,
    custom dependency finding code should be in (a) file(s) that can be
    installed alongside the package config file.
    Also, to have a consistent way in which we find dependencies.
    
    Therefore: add Find modules for all dependencies for which CMake does
    not provide one, or where we have additional finding code that needs to
    be wrapped. And, always use find_package within the main CMakeLists to
    find a dependency.
    
    * CMake: Add a package config
    
    Instead of consuming projects needing to define a find module for occa,
    a package config is provided by occa itself. Thus all occa targets are
    correctly imported during a find_package(occa), including the
    information on their dependencies. And in the package config it is
    defined what targets are being made available.
    
    Consumers can then go directly to
    target_link_libraries(theirTarget OCCA::libocca).
    
    The occa binary is also exported as a target, because a consuming
    project may want to use it in e.g. a custom command.
    
    * Cmake: Improve relocatability by using imported targets
    
    For Threads, OpenMP, (CUDA,) OpenCL and MPI, use the IMPORTED targets provided by the
    Cmake Find modules, instead of path-specific variables, to link to.
    For DPCPP, HIP and METAL, create an IMPORTED INTERFACE library ourselves, as it is not provided.
    
    * Cmake: fix casing of AND in if
    
    * Cmake: Only look for OPENCL_ROOT if not already set
    
    * Cmake: FindHIP fix compile definition
    
    The target_property INTERFACE_COMPILE_DEFINITIONS should not have a leading -D, only the define itself.
    
    Co-authored-by: Noel Chalmers <noel.chalmers@gmail.com>
    
    Co-authored-by: Noel Chalmers <noel.chalmers@gmail.com>

---
+++ cmake/FindDPCPP.cmake
@@ -0,0 +1,37 @@
+###############################################################################
+# FIND module for DPCPP components
+# This Find module is also distributed alongside the occa package config file!
+###############################################################################
+
+message(CHECK_START "Looking for DPC++")
+unset(missingDpcppComponents)
+
+find_path(
+  SYCL_INCLUDE_DIRS
+  NAMES
+    CL/sycl.hpp
+)
+
+find_library(
+  SYCL_LIBRARIES
+  NAMES
+    sycl libsycl
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+    DPCPP
+    REQUIRED_VARS
+    SYCL_INCLUDE_DIRS
+    SYCL_LIBRARIES
+    )
+
+if(DPCPP_FOUND AND NOT TARGET OCCA::depends::DPCPP)
+  # Create our wrapper imported target
+  # Put it in the OCCA namespace to make it clear that we created it.
+  add_library(OCCA::depends::DPCPP INTERFACE IMPORTED)
+  set_target_properties(OCCA::depends::DPCPP PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${SYCL_INCLUDE_DIRS}"
+    INTERFACE_LINK_LIBRARIES "${SYCL_LIBRARIES}"
+  )
+endif()
