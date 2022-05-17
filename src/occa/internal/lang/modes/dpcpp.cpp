0d06a5d5da8bb0b89f911712abc715ac9723f844
Kris Rowe <kris.rowe@anl.gov>
2022-05-16 23:50:47 -0500

    Enable math functions (#577)
    * Add tests for common math functions.
    * Include `sycl` namespace in translated kernels to use math functions.

--- src/occa/internal/lang/modes/dpcpp.cpp
+++ src/occa/internal/lang/modes/dpcpp.cpp
@@ -96,7 +96,7 @@ namespace occa
         root.addFirst(
             *(new directiveStatement(
                 &root,
-                directiveToken(root.source->origin, "include <CL/sycl.hpp>"))));
+                directiveToken(root.source->origin, "include <CL/sycl.hpp>\n using namespace sycl;\n"))));
       }
 
       void dpcppParser::addExtensions()
