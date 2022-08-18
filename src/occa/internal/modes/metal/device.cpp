a9103f0cbc960c904b307267552b80a7bfdf45c5
Stefan Frijters <stefan.frijters@vortech.nl>
2022-08-18 18:47:55 +0200

    Show compiler output for kernel if verbose is true (#619)
    This behaviour was changed for CUDA in the v1.3 release, but getting e.g.
    information about register spilling is very useful.
    
    Also implement similar behaviour for HIP and Metal.

--- src/occa/internal/modes/metal/device.cpp
+++ src/occa/internal/modes/metal/device.cpp
@@ -176,6 +176,8 @@ namespace occa {
               << "Output:\n\n"
               << commandOutput << "\n"
             );
+          } else if (verbose) {
+              io::stdout << "Output:\n\n" << commandOutput << "\n";
           }
 
           return true;
@@ -212,6 +214,8 @@ namespace occa {
           << "Output:\n\n"
           << commandOutput << "\n"
         );
+      } else if (verbose) {
+          io::stdout << "Output:\n\n" << commandOutput << "\n";
       }
       //================================
     }
