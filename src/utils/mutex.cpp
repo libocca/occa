6488cc3e9b6c8c518a776c6e7414fd0bcba03d68
nnunn <nnnunnn@gmail.com>
2023-01-20 20:46:26 +1100

    clarify definition of mutex_t for msvc

--- src/utils/mutex.cpp
+++ src/utils/mutex.cpp
@@ -1,10 +1,17 @@
 #include <occa/utils/mutex.hpp>
 #include <occa/utils/logging.hpp>
 
+#if (OCCA_USING_VS)
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif
+
 namespace occa {
+
+#if (OCCA_OS & (OCCA_LINUX_OS | OCCA_MACOS_OS))
+
   mutex_t::mutex_t() :
     mutexHandle(PTHREAD_MUTEX_INITIALIZER) {
-#if (OCCA_OS & (OCCA_LINUX_OS | OCCA_MACOS_OS))
     int error = pthread_mutex_init(&mutexHandle, NULL);
 #if OCCA_UNSAFE
     ignoreResult(error);
@@ -12,10 +19,17 @@ namespace occa {
 
     OCCA_ERROR("Error initializing mutex",
                error == 0);
-#else
-    mutexHandle = CreateMutex(NULL, FALSE, NULL);
-#endif
   }
+
+#else
+
+  mutex_t::mutex_t() {
+    mutexHandle = (void*)CreateMutex(NULL, FALSE, NULL);
+    OCCA_ERROR("Error initializing mutex",
+               mutexHandle != NULL);
+  }
+    
+#endif
 
   void mutex_t::free() {
 #if (OCCA_OS & (OCCA_LINUX_OS | OCCA_MACOS_OS))
@@ -26,8 +40,13 @@ namespace occa {
 
     OCCA_ERROR("Error freeing mutex",
                error == 0);
+
 #else
-    CloseHandle(mutexHandle);
+
+    BOOL bOK = CloseHandle(mutexHandle);
+    OCCA_ERROR("Error freeing mutex",
+               bOK != 0);
+
 #endif
   }
 
