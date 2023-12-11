67547d6bc933d1b5c598a56897ece0a86aad0f1d
Kris Rowe <kris.rowe@anl.gov>
2023-12-11 19:41:24 +0000

    Test memory copies in core unit tests.

--- tests/src/core/memory.cpp
+++ tests/src/core/memory.cpp
@@ -2,12 +2,16 @@
 #include <occa/internal/utils/testing.hpp>
 
 void testMalloc();
+void testCopy();
+void testPartialCopy();
 void testSlice();
 void testUnwrap();
 void testCast();
 
 int main(const int argc, const char **argv) {
   testMalloc();
+  testCopy();
+  testPartialCopy();
   testSlice();
   testUnwrap();
   testCast();
@@ -170,4 +174,87 @@ void testCast() {
   ASSERT_TRUE(occa::dtype::byte == casted_memory.dtype());
 
   ASSERT_EQ(occa_memory.size(), casted_memory.size());
+}
+
+void testCopy() {
+  occa::device occa_device({{"mode", "Serial"}});
+
+  const std::size_t N = 1024;
+
+  std::vector<int> x_host(N,1);
+  std::vector<int> y1_host(N,0);
+  std::vector<int> y2_host(N,0);
+
+  occa::memory x_device = occa_device.malloc<int>(N);
+  occa::memory y_device = occa_device.malloc<int>(N);
+
+  x_device.copyFrom(x_host.data());
+  x_device.copyTo(y1_host.data());
+  for (const auto& y : y1_host) {ASSERT_EQ(y,1);}
+
+  y_device.copyFrom(x_device);
+  y_device.copyTo(y2_host.data());
+  for (const auto& y : y2_host) {ASSERT_EQ(y,1);}
+}
+
+void testPartialCopy() {
+  occa::device occa_device({{"mode", "Serial"}});
+
+  const std::size_t N = 1024 * 2 * 3 * 4;
+
+  std::vector<int> x_host(N);
+  std::vector<int> y_host(N);
+
+  occa::memory x_device = occa_device.malloc<int>(N);
+  occa::memory y_device = occa_device.malloc<int>(N);
+
+  for (std::size_t n = 0; n < N; ++n) {
+    x_host[n] = static_cast<int>(n);
+  }
+ 
+  std::size_t entries;
+  std::size_t offset;
+
+  //---[Host to device]-------
+  // Last quarter
+  entries = N/4;
+  offset = 3*N/4;
+  x_device.copyFrom(x_host.data() + offset, entries, offset);
+
+  // First quarter
+  offset = 0;
+  x_device.copyFrom(x_host.data() + offset, entries, offset);
+
+  // Middle-half
+  entries = N/2;
+  offset = N/4;
+  x_device.copyFrom(x_host.data() + offset, entries, offset);
+
+  //---[Device to device]-------
+  // Middle third
+  entries = N/3;
+  offset = N/3;
+  y_device.copyFrom(x_device, entries, offset, offset);
+
+  // First third
+  offset = 0;
+  x_device.copyTo(y_device, entries, offset, offset);
+
+  // Last third
+  offset = 2 * N / 3;
+  y_device.copyFrom(x_device, entries, offset, offset);
+
+  //---[Device to host]-------
+  // Last half
+  entries = N/2;
+  offset = N/2;
+  y_device.copyTo(y_host.data() + offset, entries, offset);
+
+  // First half
+  offset = 0;
+  y_device.copyTo(y_host.data() + offset, entries, offset);
+
+  for (std::size_t n = 0; n < N; ++n) {
+    ASSERT_EQ(x_host[n], y_host[n]);
+  }
 }
