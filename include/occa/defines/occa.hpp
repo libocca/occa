8f107697bc8aa9fa725b981018bb086c3cb4fa2d
Kris Rowe <kris.rowe@anl.gov>
2023-12-18 19:41:42 +0000

    Update version number.

--- include/occa/defines/occa.hpp
+++ include/occa/defines/occa.hpp
@@ -3,17 +3,17 @@
 
 #include <occa/defines/compiledDefines.hpp>
 
-#define OCCA_MAJOR_VERSION 1
-#define OCCA_MINOR_VERSION 6
+#define OCCA_MAJOR_VERSION 2
+#define OCCA_MINOR_VERSION 0
 #define OCCA_PATCH_VERSION 0
-#define OCCA_VERSION       10600
-#define OCCA_VERSION_STR   "1.6.0"
+#define OCCA_VERSION       20000
+#define OCCA_VERSION_STR   "2.0.0"
 
-#define OKL_MAJOR_VERSION 1
-#define OKL_MINOR_VERSION 6
+#define OKL_MAJOR_VERSION 2
+#define OKL_MINOR_VERSION 0
 #define OKL_PATCH_VERSION 0
-#define OKL_VERSION       10600
-#define OKL_VERSION_STR   "1.6.0"
+#define OKL_VERSION       20000
+#define OKL_VERSION_STR   "2.0.0"
 
 #define OCCA_DEFAULT_MEM_BYTE_ALIGN 32
 
