99d1efd9af10fba421dec4f0488221ef1c3c53ec
Thilina Ratnayaka <thilinarmtb@gmail.com>
2025-06-14 20:33:59 +0000

    Error out in configuration if compiler doesn't support SYCL

--- cmake/CheckRequiredCompilerFeatures.cmake
+++ cmake/CheckRequiredCompilerFeatures.cmake
@@ -13,3 +13,10 @@ int main(int argc, char *argv[]) {
 if (NOT HAS_FILE_SYSTEM)
   message(FATAL_ERROR "CXX compiler doesn't have std::filesystem support !")
 endif()
+
+if (OCCA_DPCPP_ENABLED)
+  check_cxx_compiler_flag("-fsycl" IS_SYCL_SUPPORTED)
+  if (NOT IS_SYCL_SUPPORTED)
+    message(FATAL_ERROR "DPCPP is enabled but the compiler doesn't support \"-fsycl\" flag!")
+  endif()
+endif()
