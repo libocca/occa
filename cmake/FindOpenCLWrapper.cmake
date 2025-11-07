11c7b22eba879a879c279f27ff75ccefebaf2421
Thilina Ratnayaka <thilinarmtb@users.noreply.github.com>
2025-11-07 14:55:35 -0600

    Format CMake files with `cmake-format` (#807)
    * Format CMake files with `cmake-format`
    * Add a format check to GitHub CI
    * Run cff-validator on a PR as well

--- cmake/FindOpenCLWrapper.cmake
+++ cmake/FindOpenCLWrapper.cmake
@@ -1,51 +1,51 @@
-###############################################################################
-# FIND module wrapper around finding OpenCL
-# This Find module is also distributed alongside the occa package config file!
-###############################################################################
+# ##############################################################################
+# FIND module wrapper around finding OpenCL This Find module is also distributed
+# alongside the occa package config file!
+# ##############################################################################
 
 # Try finding OpenCL. The user should set OpenCL_ROOT if needed.
 find_package(OpenCL QUIET)
 if(NOT OpenCL_FOUND)
   # Otherwise, look for the headers and library in standard locations
-  find_path(OpenCL_INCLUDE_DIR
+  find_path(
+    OpenCL_INCLUDE_DIR
     NAMES CL/cl.h OpenCL/cl.h
-    PATHS
-      ENV CUDA_PATH
-      ENV CUDAToolkit_ROOT
-      ENV ROCM_PATH
-      ENV NVHPC_ROOT
-      ENV SYCL_ROOT
-      /usr/local/cuda
-      /opt/rocm/
-      /opt/rocm/opencl
-      /opt/intel/oneapi/compiler/latest/linux
-    PATH_SUFFIXES
-      include
-      include/sycl
-  )
+    PATHS ENV
+          CUDA_PATH
+          ENV
+          CUDAToolkit_ROOT
+          ENV
+          ROCM_PATH
+          ENV
+          NVHPC_ROOT
+          ENV
+          SYCL_ROOT
+          /usr/local/cuda
+          /opt/rocm/
+          /opt/rocm/opencl
+          /opt/intel/oneapi/compiler/latest/linux
+    PATH_SUFFIXES include include/sycl)
 
-  find_library(OpenCL_LIBRARY
+  find_library(
+    OpenCL_LIBRARY
     NAMES OpenCL libOpenCL
-    PATHS
-      ENV CUDA_PATH
-      ENV CUDAToolkit_ROOT
-      ENV ROCM_PATH
-      ENV NVHPC_ROOT
-      ENV SYCL_ROOT
-      /usr/local/cuda
-      /opt/rocm/
-      /opt/rocm/opencl
-      /opt/intel/oneapi/compiler/latest/linux
-    PATH_SUFFIXES
-      lib
-      lib64
-  )
+    PATHS ENV
+          CUDA_PATH
+          ENV
+          CUDAToolkit_ROOT
+          ENV
+          ROCM_PATH
+          ENV
+          NVHPC_ROOT
+          ENV
+          SYCL_ROOT
+          /usr/local/cuda
+          /opt/rocm/
+          /opt/rocm/opencl
+          /opt/intel/oneapi/compiler/latest/linux
+    PATH_SUFFIXES lib lib64)
 endif()
 find_package(OpenCL)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(
-    OpenCLWrapper
-    REQUIRED_VARS
-    OpenCL_FOUND
-    )
+find_package_handle_standard_args(OpenCLWrapper REQUIRED_VARS OpenCL_FOUND)
